17de1b50193a934a4bc8f0e2d0dc3c5f3ce4ed57
jstrait <joel.strait@gmail.com>
2014-11-04 09:45:20 +0700

    Extracting next coordinate selection logic from rotation animation
    Removing some global variables in the process

--- camera.js
+++ camera.js
@@ -7,9 +7,7 @@ var targetX;
 var targetGridX;
 var targetZ;
 var targetGridZ;
-var targetAngle = 0;
 var ROTATION_DELTA = 0.03;
-var deltaAngle = ROTATION_DELTA;
 
 var AnimationManager = function() {
   var TARGET_FRAME_WINDOW = 1000 / 60;   // 60 frames per second
@@ -17,6 +15,9 @@ var AnimationManager = function() {
   var animationManager = {};
   var animators = [];
   var previousFrameTimestamp;
+
+  var targetAngle = 0;
+  var deltaAngle = ROTATION_DELTA;
 
   var init = function() {
     targetX = 0;
@@ -27,6 +28,55 @@ var AnimationManager = function() {
     var ramp = new rampAnimation((camera.position.z - (city.HALF_SCENE_DEPTH + (city.STREET_WIDTH / 2))) / Math.abs(deltaZ), -SWOOP_DESCENT_DELTA, 0.5, 1000000);
     var forward = new forwardAnimation();
     animators = [ramp, forward];
+  };
+
+  var determineNextTargetPoint = function() {
+    if (targetGridX == undefined || targetGridZ == undefined) {
+      targetGridX = 0;
+      targetGridZ = -1;
+    }
+    else {
+      var oldTargetGridX = targetGridX;
+      var oldTargetGridZ = targetGridZ;
+
+      while (oldTargetGridX == targetGridX && oldTargetGridZ == targetGridZ) {
+        if (deltaX == 0) {
+          targetGridX = Math.floor(Math.random() * city.BLOCK_ROWS) - (city.BLOCK_ROWS / 2);
+        }
+        else if (deltaZ == 0) {
+          targetGridZ = Math.floor(Math.random() * city.BLOCK_COLUMNS) - (city.BLOCK_COLUMNS / 2);
+        }
+      }
+    }
+
+    targetX = (targetGridX * city.BLOCK_WIDTH) + (targetGridX * city.STREET_WIDTH);
+    targetZ = (targetGridZ * city.BLOCK_WIDTH) + (targetGridZ * city.STREET_WIDTH);
+
+    deltaX = (camera.position.x == targetX) ? 0 : 0.2;
+    deltaZ = (camera.position.z == targetZ) ? 0 : 0.2;
+    deltaX *= (camera.position.x > targetX) ? -1 : 1;
+    deltaZ *= (camera.position.z > targetZ) ? -1 : 1;
+  };
+
+  var determineRotationAngle = function() {
+    var RIGHT_ANGLE = Math.PI / 2;
+
+    var oldTargetAngle = targetAngle;
+    if (deltaX != 0 && oldTargetAngle == 0) {  // NORTH
+      targetAngle = (deltaX < 0) ? targetAngle + RIGHT_ANGLE : targetAngle - RIGHT_ANGLE;
+    }
+    else if (deltaZ != 0 && (oldTargetAngle == RIGHT_ANGLE || oldTargetAngle == ((-3 * Math.PI) / 2))) {  // EAST
+      targetAngle = (deltaZ < 0) ? targetAngle - RIGHT_ANGLE : targetAngle + RIGHT_ANGLE;
+    }
+    else if (deltaX != 0 && (oldTargetAngle == Math.PI || oldTargetAngle == Math.PI * -1)) {  // SOUTH
+      targetAngle = (deltaX < 0) ? targetAngle - RIGHT_ANGLE : targetAngle + RIGHT_ANGLE;
+    }
+    else if (deltaZ != 0 && (oldTargetAngle == Math.PI / -2 || oldTargetAngle == (Math.PI * 3) / 2)) {  // WEST
+      targetAngle = (deltaZ < 0) ? targetAngle + RIGHT_ANGLE : targetAngle - RIGHT_ANGLE;
+    }
+
+    deltaAngle = ROTATION_DELTA;
+    deltaAngle *= (targetAngle > oldTargetAngle) ? 1 : -1;
   };
 
   animationManager.animate = function() {
@@ -57,7 +107,9 @@ var AnimationManager = function() {
           newAnimators.push(new hoverAnimation());
         }
         else if (animators[i] instanceof forwardAnimation) {
-          newAnimators.push(new rotationAnimation());
+          determineNextTargetPoint();
+          determineRotationAngle();
+          newAnimators.push(new rotationAnimation(targetAngle, deltaAngle));
         }
         else if (animators[i] instanceof rotationAnimation) {
           newAnimators.push(new forwardAnimation());
@@ -97,64 +149,21 @@ forwardAnimation.prototype.animate = function(frameCount) {
   }
 }
 
-function rotationAnimation() {
-  var RIGHT_ANGLE = Math.PI / 2;
-
-  if (targetGridX == undefined || targetGridZ == undefined) {
-    targetGridX = 0;
-    targetGridZ = -1;
-  }
-  else {
-    var oldTargetGridX = targetGridX;
-    var oldTargetGridZ = targetGridZ;
-
-    while (oldTargetGridX == targetGridX && oldTargetGridZ == targetGridZ) {
-      if (deltaX == 0) {
-        targetGridX = Math.floor(Math.random() * city.BLOCK_ROWS) - (city.BLOCK_ROWS / 2);
-      }
-      else if (deltaZ == 0) {
-        targetGridZ = Math.floor(Math.random() * city.BLOCK_COLUMNS) - (city.BLOCK_COLUMNS / 2);
-      }
-    }
-  }
-
-  targetX = (targetGridX * city.BLOCK_WIDTH) + (targetGridX * city.STREET_WIDTH);
-  targetZ = (targetGridZ * city.BLOCK_WIDTH) + (targetGridZ * city.STREET_WIDTH);
-  
-  deltaX = (camera.position.x == targetX) ? 0 : 0.2;
-  deltaZ = (camera.position.z == targetZ) ? 0 : 0.2;
-  deltaX *= (camera.position.x > targetX) ? -1 : 1;
-  deltaZ *= (camera.position.z > targetZ) ? -1 : 1;
-
-  var oldTargetAngle = targetAngle;
-  if (deltaX != 0 && oldTargetAngle == 0) {  // NORTH
-    targetAngle = (deltaX < 0) ? targetAngle + RIGHT_ANGLE : targetAngle - RIGHT_ANGLE;
-  }
-  else if (deltaZ != 0 && (oldTargetAngle == RIGHT_ANGLE || oldTargetAngle == ((-3 * Math.PI) / 2))) {  // EAST
-    targetAngle = (deltaZ < 0) ? targetAngle - RIGHT_ANGLE : targetAngle + RIGHT_ANGLE;
-  }
-  else if (deltaX != 0 && (oldTargetAngle == Math.PI || oldTargetAngle == Math.PI * -1)) {  // SOUTH
-    targetAngle = (deltaX < 0) ? targetAngle - RIGHT_ANGLE : targetAngle + RIGHT_ANGLE;
-  }
-  else if (deltaZ != 0 && (oldTargetAngle == Math.PI / -2 || oldTargetAngle == (Math.PI * 3) / 2)) {  // WEST
-    targetAngle = (deltaZ < 0) ? targetAngle + RIGHT_ANGLE : targetAngle - RIGHT_ANGLE;
-  }
-
-  deltaAngle = ROTATION_DELTA;
-  deltaAngle *= (targetAngle > oldTargetAngle) ? 1 : -1;
-
+function rotationAnimation(targetAngle, deltaAngle) {
+  this.targetAngle = targetAngle;
+  this.deltaAngle = deltaAngle;
   this.finished = false;
 }
 
 rotationAnimation.prototype.animate = function(frameCount) {
-  camera.rotation.y += deltaAngle * frameCount;
+  camera.rotation.y += this.deltaAngle * frameCount;
   
-  if ((deltaAngle < 0 && camera.rotation.y <= targetAngle) || (deltaAngle > 0 && camera.rotation.y >= targetAngle)) {
-    if (targetAngle >= Math.PI * 2 || targetAngle <= Math.PI * -2) {
-      targetAngle = 0;
+  if ((this.deltaAngle < 0 && camera.rotation.y <= this.targetAngle) || (this.deltaAngle > 0 && camera.rotation.y >= this.targetAngle)) {
+    if (this.targetAngle >= Math.PI * 2 || this.targetAngle <= Math.PI * -2) {
+      this.targetAngle = 0;
     }
 
-    camera.rotation.y = targetAngle;
+    camera.rotation.y = this.targetAngle;
     this.finished = true;
   }
 }
