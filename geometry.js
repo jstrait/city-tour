84fd03931f84eabb15609461f26f9ae8fa88972c
jstrait <joel.strait@gmail.com>
2014-11-07 10:17:16 +0700

    Changing style of City class definition
    Allowing private methods

--- geometry.js
+++ geometry.js
@@ -1,19 +1,21 @@
-function City() {
-  this.STREET_WIDTH = 2;
-  this.STREET_DEPTH = 2;
-  this.BLOCK_WIDTH = 4;
-  this.BLOCK_DEPTH = 4;
-  this.BLOCK_ROWS = 60;
-  this.BLOCK_COLUMNS = 20;
-  this.MIN_BUILDING_HEIGHT = 0.5;
-  this.MAX_BUILDING_HEIGHT = 20;
-  this.MAX_BUILDING_MATERIALS = 50;
-  this.TOTAL_SCENE_WIDTH = (this.BLOCK_WIDTH * this.BLOCK_ROWS) + (this.STREET_WIDTH * (this.BLOCK_ROWS - 1));
-  this.HALF_SCENE_WIDTH = this.TOTAL_SCENE_WIDTH / 2;
-  this.TOTAL_SCENE_DEPTH = (this.BLOCK_DEPTH * this.BLOCK_COLUMNS) + (this.STREET_DEPTH * (this.BLOCK_COLUMNS - 1));
-  this.HALF_SCENE_DEPTH = this.TOTAL_SCENE_DEPTH / 2;
+var City = function() {
+  var city = {};
 
-  this.BLOCK_LAYOUTS = [
+  city.STREET_WIDTH = 2;
+  city.STREET_DEPTH = 2;
+  city.BLOCK_WIDTH = 4;
+  city.BLOCK_DEPTH = 4;
+  city.BLOCK_ROWS = 60;
+  city.BLOCK_COLUMNS = 20;
+  city.MIN_BUILDING_HEIGHT = 0.5;
+  city.MAX_BUILDING_HEIGHT = 20;
+  city.MAX_BUILDING_MATERIALS = 50;
+  city.TOTAL_SCENE_WIDTH = (city.BLOCK_WIDTH * city.BLOCK_ROWS) + (city.STREET_WIDTH * (city.BLOCK_ROWS - 1));
+  city.HALF_SCENE_WIDTH = city.TOTAL_SCENE_WIDTH / 2;
+  city.TOTAL_SCENE_DEPTH = (city.BLOCK_DEPTH * city.BLOCK_COLUMNS) + (city.STREET_DEPTH * (city.BLOCK_COLUMNS - 1));
+  city.HALF_SCENE_DEPTH = city.TOTAL_SCENE_DEPTH / 2;
+
+  city.BLOCK_LAYOUTS = [
                         [ { width: 1.0, depth: 1.0, offsetFromBlockCenterX: 0.0, offsetFromBlockCenterZ: 0.0 } ],
                         [ { width: 0.5, depth: 1.0, offsetFromBlockCenterX: -0.5, offsetFromBlockCenterZ: 0.0 },
                           { width: 0.5, depth: 1.0, offsetFromBlockCenterX: 0.5, offsetFromBlockCenterZ: 0.0 } ],
@@ -32,80 +34,81 @@ function City() {
                           { width: 0.5, depth: 0.5, offsetFromBlockCenterX: -0.5, offsetFromBlockCenterZ: -0.5},
                           { width: 0.5, depth: 0.5, offsetFromBlockCenterX: 0.5, offsetFromBlockCenterZ: -0.5} ],
                       ];
-}
 
-City.prototype.buildScene = function() {
-  var scene = new THREE.Scene();
+  city.buildScene = function() {
+    var scene = new THREE.Scene();
 
-  var groundMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
-  var ground = new THREE.Mesh(new THREE.PlaneGeometry(this.TOTAL_SCENE_WIDTH * 25, this.TOTAL_SCENE_DEPTH * 25), groundMaterial);
-  ground.rotation.x = -(Math.PI / 2);
-  scene.add(ground);
+    var groundMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+    var ground = new THREE.Mesh(new THREE.PlaneGeometry(city.TOTAL_SCENE_WIDTH * 25, city.TOTAL_SCENE_DEPTH * 25), groundMaterial);
+    ground.rotation.x = -(Math.PI / 2);
+    scene.add(ground);
 
-  var buildingMaterials = this.buildMaterials();
-  var buildingGeometries = this.buildGeometries();
-  
-  // Loop through the lower left corner of each block
-  for (var i = -(this.HALF_SCENE_WIDTH); i < this.HALF_SCENE_WIDTH; i += this.BLOCK_WIDTH + this.STREET_WIDTH) {
-    for (var j = -(this.HALF_SCENE_DEPTH); j < this.HALF_SCENE_DEPTH; j += this.BLOCK_DEPTH + this.STREET_DEPTH) {
+    var buildingMaterials = buildMaterials();
+    var buildingGeometries = buildGeometries();
 
-      var building;
-      var blockLayout = this.BLOCK_LAYOUTS[Math.floor(Math.random() * this.BLOCK_LAYOUTS.length)];
-      for (var layoutIndex = 0; layoutIndex < blockLayout.length; layoutIndex++) {
-        var lotLayout = blockLayout[layoutIndex];
-        var maxHeightI = this.MAX_BUILDING_HEIGHT * ((this.HALF_SCENE_WIDTH - Math.abs(i)) / this.HALF_SCENE_WIDTH);
-        var maxHeightJ = this.MAX_BUILDING_HEIGHT * ((this.HALF_SCENE_DEPTH - Math.abs(j)) / this.HALF_SCENE_DEPTH);
-        var maxHeight = Math.min(maxHeightI, maxHeightJ);
-        var buildingHeight = Math.floor((Math.random() * maxHeight - this.MIN_BUILDING_HEIGHT)) + this.MIN_BUILDING_HEIGHT;
+    // Loop through the lower left corner of each block
+    for (var i = -(city.HALF_SCENE_WIDTH); i < city.HALF_SCENE_WIDTH; i += city.BLOCK_WIDTH + city.STREET_WIDTH) {
+      for (var j = -(city.HALF_SCENE_DEPTH); j < city.HALF_SCENE_DEPTH; j += city.BLOCK_DEPTH + city.STREET_DEPTH) {
 
-        var building = this.generateBuilding(i, maxHeight, j, lotLayout);
+        var building;
+        var blockLayout = city.BLOCK_LAYOUTS[Math.floor(Math.random() * city.BLOCK_LAYOUTS.length)];
+        for (var layoutIndex = 0; layoutIndex < blockLayout.length; layoutIndex++) {
+          var lotLayout = blockLayout[layoutIndex];
+          var maxHeightI = city.MAX_BUILDING_HEIGHT * ((city.HALF_SCENE_WIDTH - Math.abs(i)) / city.HALF_SCENE_WIDTH);
+          var maxHeightJ = city.MAX_BUILDING_HEIGHT * ((city.HALF_SCENE_DEPTH - Math.abs(j)) / city.HALF_SCENE_DEPTH);
+          var maxHeight = Math.min(maxHeightI, maxHeightJ);
+          var buildingHeight = Math.floor((Math.random() * maxHeight - city.MIN_BUILDING_HEIGHT)) + city.MIN_BUILDING_HEIGHT;
 
-        var index = Math.floor(Math.random() * this.MAX_BUILDING_MATERIALS);
-        THREE.GeometryUtils.merge(buildingGeometries[index], building); 
+          var building = generateBuilding(i, maxHeight, j, lotLayout);
+
+          var index = Math.floor(Math.random() * city.MAX_BUILDING_MATERIALS);
+          THREE.GeometryUtils.merge(buildingGeometries[index], building);
+        }
       }
     }
-  }
 
-  for (var i = 0; i < this.MAX_BUILDING_MATERIALS; i++) {
-    scene.add(new THREE.Mesh(buildingGeometries[i], buildingMaterials[i]));
-  }
+    for (var i = 0; i < city.MAX_BUILDING_MATERIALS; i++) {
+      scene.add(new THREE.Mesh(buildingGeometries[i], buildingMaterials[i]));
+    }
 
-  return scene;
-}
+    return scene;
+  };
 
-City.prototype.buildMaterials = function() {
-  var buildingMaterials = [];
+  var buildMaterials = function() {
+    var buildingMaterials = [];
 
-  for (var i = 0; i < this.MAX_BUILDING_MATERIALS; i++) {
-    var r = Math.random();
-    var g = r;
-    var b = r;
-    var buildingColor = new THREE.Color(0xffffff).setRGB(r, g, b);
+    for (var i = 0; i < city.MAX_BUILDING_MATERIALS; i++) {
+      var r = Math.random();
+      var g = r;
+      var b = r;
+      var buildingColor = new THREE.Color(0xffffff).setRGB(r, g, b);
 
-    buildingMaterials.push(new THREE.MeshLambertMaterial({ color: buildingColor.getHex() }));
-  }
+      buildingMaterials.push(new THREE.MeshLambertMaterial({ color: buildingColor.getHex() }));
+    }
 
-  return buildingMaterials;
-}
+    return buildingMaterials;
+  };
 
-City.prototype.buildGeometries = function() {
-  var buildingGeometries = [];
+  var buildGeometries = function() {
+    var buildingGeometries = [];
 
-  for (var i = 0; i < this.MAX_BUILDING_MATERIALS; i++) {
-    buildingGeometries.push(new THREE.Geometry());
-  }
+    for (var i = 0; i < city.MAX_BUILDING_MATERIALS; i++) {
+      buildingGeometries.push(new THREE.Geometry());
+    }
 
-  return buildingGeometries;
-}
+    return buildingGeometries;
+  };
 
-City.prototype.generateBuilding = function(x, maxY, z, lotLayout) {
-  var building = new THREE.Mesh(new THREE.BoxGeometry(lotLayout.width * this.BLOCK_WIDTH, 1, lotLayout.depth * this.BLOCK_WIDTH));
+  var generateBuilding = function(x, maxY, z, lotLayout) {
+    var building = new THREE.Mesh(new THREE.BoxGeometry(lotLayout.width * city.BLOCK_WIDTH, 1, lotLayout.depth * city.BLOCK_WIDTH));
 
-  building.position.x = x + this.BLOCK_WIDTH / 2 + ((this.BLOCK_WIDTH / 2) * lotLayout.offsetFromBlockCenterX);
-  building.scale.y =  (Math.random() * maxY) + this.MIN_BUILDING_HEIGHT;
-  building.position.y = building.scale.y / 2;
-  building.position.z = z + this.BLOCK_DEPTH / 2 + ((this.BLOCK_DEPTH / 2) * lotLayout.offsetFromBlockCenterZ);
+    building.position.x = x + city.BLOCK_WIDTH / 2 + ((city.BLOCK_WIDTH / 2) * lotLayout.offsetFromBlockCenterX);
+    building.scale.y =  (Math.random() * maxY) + city.MIN_BUILDING_HEIGHT;
+    building.position.y = building.scale.y / 2;
+    building.position.z = z + city.BLOCK_DEPTH / 2 + ((city.BLOCK_DEPTH / 2) * lotLayout.offsetFromBlockCenterZ);
 
-  return building;
-}
+    return building;
+  };
 
+  return city;
+};
