6236b2bec88fcd5ffe8705f3506f4b769a800d78
Joel Strait <joel.strait@gmail.com>
2016-07-01 23:29:50 -0700

    Adding new block layout

--- src/buildings.js
+++ src/buildings.js
@@ -70,6 +70,26 @@ var Buildings = function(terrain) {
               { left:     0.4167, right: 0.5834,  top: 0.5,  bottom:  1.0, },
               { left:     0.5834, right: 0.75,    top: 0.5,  bottom:  1.0, }, ],
     },
+
+    {
+      maxBlockSteepness: 1000000,
+      lots: [ { left:     0.0,  right: 0.5,    top: 0.0,    bottom:  (1/3), },
+              { left:     0.0,  right: 0.5,    top: (1/3),  bottom:  0.5, },
+              { left:     0.0,  right: 0.5,    top: 0.5,    bottom:  (2/3), },
+              { left:     0.0,  right: (1/3),  top: (2/3),  bottom:  0.8333, },
+              { left:     0.0,  right: (1/3),  top: 0.8333, bottom:  1.0, },
+              
+              { left:     0.5,  right: 1.0,    top: 0.0,    bottom:  0.1667, },
+              { left:     0.5,  right: 1.0,    top: 0.1677, bottom:  (1/3), },
+              { left:     0.5,  right: 1.0,    top: (1/3),  bottom:  0.5, },
+              { left:     0.5,  right: 1.0,    top: 0.5,    bottom:  (2/3), },
+
+              { left:     (1/3),  right: (2/3),  top: (2/3),    bottom:  1.0, },
+              { left:     (2/3),  right: 0.8333, top: (2/3),    bottom:  1.0, },
+              { left:    0.8333,  right: 1.0,    top: (2/3),    bottom:  1.0, },
+              
+            ],
+    },
   ];
 
   var generateUnitBlocks = function(terrain) {
