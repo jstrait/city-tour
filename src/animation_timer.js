90bbc9a2c50cc79600fa0a869faf32bd2a6c2c49
Joel Strait <joel.strait@gmail.com>
2016-09-06 22:13:36 -0700

    Extracting AnimationTimer function to its own file

---
+++ src/animation_timer.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var AnimationTimer = function() {
+  var FRAMES_PER_SECONDS = 60;
+  var TARGET_FRAME_WINDOW = 1000.0 / FRAMES_PER_SECONDS;
+
+  var paused = true;
+  var previousFrameTimestamp;
+
+  var tick = function() {
+    if (paused) {
+      return;
+    }
+
+    var currentTimestamp = new Date().getTime();
+    var frameCount;
+
+    if (previousFrameTimestamp === undefined) {
+      frameCount = 1;
+    }
+    else {
+      frameCount = Math.floor((currentTimestamp - previousFrameTimestamp) / TARGET_FRAME_WINDOW);
+      if (frameCount < 1) {
+        frameCount = 1;
+      }
+    }
+    previousFrameTimestamp = currentTimestamp;
+
+    animationTimer.onAnimate(frameCount);
+
+    requestAnimFrame(tick);
+  };
+
+  var start = function() {
+    paused = false;
+    previousFrameTimestamp = undefined;
+    requestAnimFrame(tick);
+  };
+
+  var pause = function() {
+    paused = true;
+  };
+
+  var animationTimer = {};
+
+  animationTimer.onAnimate = function(frameCount) {};
+
+  animationTimer.start = start;
+
+  animationTimer.togglePause = function() {
+    if (paused) {
+      start();
+    }
+    else {
+      pause();
+    }
+  };
+
+  return animationTimer;
+};
