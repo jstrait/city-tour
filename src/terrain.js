43dc1fa4cf46a350edcb5a84193b16fabd896bd3
Joel Strait <joel.strait@gmail.com>
2016-09-15 20:31:42 -0700

    Hoisting var declarations

--- src/terrain.js
+++ src/terrain.js
@@ -14,33 +14,33 @@ CityTour.Terrain = function(coordinates) {
   terrain.heightAtCoordinates = function(x, z) {
     var xIsWhole = (Math.floor(x) === x);
     var zIsWhole = (Math.floor(z) === z);
+    var leftHeight, rightHeight, topHeight, bottomHeight;
+    var topRowInterpolatedHeight, bottomRowInterpolatedHeight;
 
     if (xIsWhole && zIsWhole) {
       return coordinates[x][z];
     }
 
     if (!xIsWhole && zIsWhole) {
-      var leftHeight = coordinates[Math.floor(x)][z];
-      var rightHeight = coordinates[Math.ceil(x)][z];
+      leftHeight = coordinates[Math.floor(x)][z];
+      rightHeight = coordinates[Math.ceil(x)][z];
 
       return interpolateHeight(x, leftHeight, rightHeight);
     }
     else if (xIsWhole && !zIsWhole) {
-      var topHeight = coordinates[x][Math.floor(z)];
-      var bottomHeight = coordinates[x][Math.ceil(z)];
+      topHeight = coordinates[x][Math.floor(z)];
+      bottomHeight = coordinates[x][Math.ceil(z)];
 
       return interpolateHeight(z, topHeight, bottomHeight);
     }
     else {
-      var leftHeight = coordinates[Math.floor(x)][Math.floor(z)];
-      var rightHeight = coordinates[Math.ceil(x)][Math.floor(z)];
-
-      var topRowInterpolatedHeight = interpolateHeight(x, leftHeight, rightHeight);
+      leftHeight = coordinates[Math.floor(x)][Math.floor(z)];
+      rightHeight = coordinates[Math.ceil(x)][Math.floor(z)];
+      topRowInterpolatedHeight = interpolateHeight(x, leftHeight, rightHeight);
 
       leftHeight = coordinates[Math.floor(x)][Math.ceil(z)];
       rightHeight = coordinates[Math.ceil(x)][Math.ceil(z)];
-
-      var bottomRowInterpolatedHeight = interpolateHeight(x, leftHeight, rightHeight);
+      bottomRowInterpolatedHeight = interpolateHeight(x, leftHeight, rightHeight);
 
       return interpolateHeight(z, topRowInterpolatedHeight, bottomRowInterpolatedHeight);
     }
