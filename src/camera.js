2d7e3a7faf087ff8f35927f702f179a6b55bc94a
Joel Strait <joel.strait@gmail.com>
2016-09-24 21:37:03 -0700

    New camera rotation logic which is more flexible
    Should be able to work for rotations to any target direction,
    not just north/south/east/west.
    
    Keeping the previous implementation around for the time being
    while testing this out.

--- src/camera.js
+++ src/camera.js
@@ -42,6 +42,8 @@ CityTour.HorizontalAnimationController = function(cameraPole, pathFinder) {
     else if (oldTargetMapZ < targetMapZ) {
       deltaZ = FORWARD_MOTION_DELTA;
     }
+    
+    determineRotationAngle2(oldTargetMapX, oldTargetMapZ, targetMapX, targetMapZ);
   };
 
   var determineRotationAngle = function() {
@@ -74,6 +76,33 @@ CityTour.HorizontalAnimationController = function(cameraPole, pathFinder) {
     deltaAngle *= (targetAngle > oldTargetAngle) ? 1 : -1;
   };
 
+  var determineRotationAngle2 = function(oldTargetMapX, oldTargetMapZ, targetMapX, targetMapZ) {
+    var oldTargetAngle = targetAngle;
+
+    var x = targetMapX - oldTargetMapX;
+    var z = -(targetMapZ - oldTargetMapZ);
+    var angle = Math.atan2(z, x);
+    if (angle < HALF_PI) {
+      angle += TWO_PI;
+    }
+    var rightHandedAngle = angle - HALF_PI;
+
+    targetAngle = rightHandedAngle;
+
+    // Prevent an extra long turn (i.e. 270deg instead of 90deg)
+    if (oldTargetAngle === 0.0 && targetAngle === THREE_PI_OVER_TWO) {
+      oldTargetAngle = TWO_PI;
+      cameraPole.rotation.y = TWO_PI;
+    }
+    else if (oldTargetAngle === THREE_PI_OVER_TWO && targetAngle === 0.0) {
+      oldTargetAngle = -HALF_PI;
+      cameraPole.rotation.y = -HALF_PI;
+    }
+
+    deltaAngle = ROTATION_DELTA;
+    deltaAngle *= (targetAngle > oldTargetAngle) ? 1 : -1;
+  };
+
   var horizontalAnimationController = {};
 
   horizontalAnimationController.targetSceneX = function() { return targetSceneX; };
@@ -87,7 +116,7 @@ CityTour.HorizontalAnimationController = function(cameraPole, pathFinder) {
 
   horizontalAnimationController.nextTarget = function() {
     determineNextTargetPoint();
-    determineRotationAngle();
+    //determineRotationAngle();
   };
 
   return horizontalAnimationController;
