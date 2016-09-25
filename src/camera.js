16183443ff1b3bb76e520daeeb3fd7425421d5f9
Joel Strait <joel.strait@gmail.com>
2016-09-25 15:45:25 -0700

    Removing obsolete function that has replacement implementation

--- src/camera.js
+++ src/camera.js
@@ -111,37 +111,10 @@ CityTour.HorizontalAnimationController = function(cameraPole, pathFinder) {
     deltaX = (oldTargetMapX === targetMapX) ? 0.0 : FORWARD_MOTION_DELTA;
     deltaZ = (oldTargetMapZ === targetMapZ) ? 0.0 : FORWARD_MOTION_DELTA;
 
-    determineRotationAngle2(oldTargetMapX, oldTargetMapZ, targetMapX, targetMapZ);
+    determineRotationAngle(oldTargetMapX, oldTargetMapZ, targetMapX, targetMapZ);
   };
 
-  var determineRotationAngle = function() {
-    var oldTargetAngle = targetAngle;
-
-    if (deltaX < 0) {
-      targetAngle = HALF_PI;
-    }
-    else if (deltaX > 0) {
-      targetAngle = THREE_PI_OVER_TWO;
-    }
-    else if (deltaZ > 0) {
-      targetAngle = Math.PI;
-    }
-    else if (deltaZ < 0) {
-      targetAngle = 0;
-    }
-
-    // Prevent an extra long turn (i.e. 270deg instead of 90deg)
-    if (oldTargetAngle === 0.0 && targetAngle === THREE_PI_OVER_TWO) {
-      oldTargetAngle = TWO_PI;
-      cameraPole.rotation.y = TWO_PI;
-    }
-    else if (oldTargetAngle === THREE_PI_OVER_TWO && targetAngle === 0.0) {
-      oldTargetAngle = -HALF_PI;
-      cameraPole.rotation.y = -HALF_PI;
-    }
-  };
-
-  var determineRotationAngle2 = function(oldTargetMapX, oldTargetMapZ, targetMapX, targetMapZ) {
+  var determineRotationAngle = function(oldTargetMapX, oldTargetMapZ, targetMapX, targetMapZ) {
     var oldTargetAngle = targetAngle;
 
     var x = targetMapX - oldTargetMapX;
@@ -167,7 +140,6 @@ CityTour.HorizontalAnimationController = function(cameraPole, pathFinder) {
 
   var nextTarget = function() {
     determineNextTargetPoint();
-    //determineRotationAngle();
   };
 
   var horizontalAnimationController = {};
