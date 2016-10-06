47427d7bd96e8a557917098ba74593f50322ca84
Joel Strait <joel.strait@gmail.com>
2016-10-05 23:50:50 -0700

    Extracting constants

--- src/generators/road_network_generator.js
+++ src/generators/road_network_generator.js
@@ -3,9 +3,12 @@
 var CityTour = CityTour || {};
 
 CityTour.RoadNetworkGenerator = (function() {
-  var calculateBlockProbabilityOfBranching = function(mapX1, mapZ1, mapX2, mapZ2) {
-    var PERCENTAGE_DISTANCE_THAT_DECAY_BEGINS = 0.4;
+  var PERCENTAGE_DISTANCE_THAT_DECAY_BEGINS = 0.4;
+  var DISTANCE_TO_CITY_EDGE = Math.min(CityTour.Config.HALF_BLOCK_COLUMNS, CityTour.Config.HALF_BLOCK_ROWS);
+  var SAFE_FROM_DECAY_DISTANCE = DISTANCE_TO_CITY_EDGE * PERCENTAGE_DISTANCE_THAT_DECAY_BEGINS;
+  var MAX_STEEPNESS = Math.PI / 6;
 
+  var calculateBlockProbabilityOfBranching = function(mapX1, mapZ1, mapX2, mapZ2) {
     // Guarantee roads along x and z axes
     if (mapX1 === 0 && mapX2 === 0 && mapZ2 >= -CityTour.Config.HALF_BLOCK_ROWS && mapZ2 <= CityTour.Config.HALF_BLOCK_ROWS) {
       return 1.0;
@@ -13,15 +16,13 @@ CityTour.RoadNetworkGenerator = (function() {
     else if (mapZ1 === 0 && mapZ2 === 0 && mapX2 >= -CityTour.Config.HALF_BLOCK_COLUMNS && mapX2 <= CityTour.Config.HALF_BLOCK_COLUMNS) {
       return 1.0;
     }
-
-    var distanceToCityEdge = Math.min(CityTour.Config.HALF_BLOCK_COLUMNS, CityTour.Config.HALF_BLOCK_ROWS);
+ 
     var distanceFromCenter = Math.sqrt((mapX1 * mapX1) + (mapZ1 * mapZ1));
-    var percentageFromCenter = (distanceFromCenter / distanceToCityEdge);
+    var percentageFromCenter = (distanceFromCenter / DISTANCE_TO_CITY_EDGE);
     var normalizedPercentageFromCenter;
 
     if (percentageFromCenter >= PERCENTAGE_DISTANCE_THAT_DECAY_BEGINS) {
-      var safeFromDecayDistance = distanceToCityEdge * PERCENTAGE_DISTANCE_THAT_DECAY_BEGINS;
-      normalizedPercentageFromCenter = (distanceFromCenter - safeFromDecayDistance) / (distanceToCityEdge - safeFromDecayDistance);
+      normalizedPercentageFromCenter = (distanceFromCenter - SAFE_FROM_DECAY_DISTANCE) / (DISTANCE_TO_CITY_EDGE - SAFE_FROM_DECAY_DISTANCE);
     }
     else {
       normalizedPercentageFromCenter = 0.0;
@@ -31,8 +32,6 @@ CityTour.RoadNetworkGenerator = (function() {
   };
 
   var isTerrainTooSteep = function(terrain, mapX, mapZ, targetMapX, targetMapZ) {
-    var MAX_STEEPNESS = Math.PI / 6;
-
     var heightAtPoint1 = terrain.heightAtCoordinates(mapX, mapZ);
     var heightAtPoint2 = terrain.heightAtCoordinates(targetMapX, targetMapZ);
     var angle = Math.atan2((heightAtPoint1 - heightAtPoint2), CityTour.Config.BLOCK_DEPTH);
