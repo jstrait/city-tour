a68e7bdb6f8b1a59d6668226cb84977ba9a57329
Joel Strait <joel.strait@gmail.com>
2016-12-03 09:46:38 -0800

    Extracting config object to its own source file

--- src/city.js
+++ src/city.js
@@ -2,34 +2,6 @@
 
 var CityTour = CityTour || {};
 
-CityTour.Config = (function() {
-  var config = {};
-
-  config.STREET_WIDTH = 4;
-  config.STREET_DEPTH = 4;
-  config.SIDEWALK_WIDTH = config.STREET_WIDTH * 0.24;
-  config.SIDEWALK_DEPTH = config.STREET_DEPTH * 0.24;
-  config.ROAD_WIDTH = config.STREET_WIDTH - (config.SIDEWALK_WIDTH * 2);
-  config.ROAD_DEPTH = config.STREET_DEPTH - (config.SIDEWALK_DEPTH * 2);
-  config.BLOCK_WIDTH = 8;
-  config.BLOCK_DEPTH = 8;
-  config.BLOCK_AND_STREET_WIDTH = config.BLOCK_WIDTH + config.STREET_WIDTH;
-  config.BLOCK_AND_STREET_DEPTH = config.BLOCK_DEPTH + config.STREET_DEPTH;
-  config.BLOCK_ROWS = 64;
-  config.BLOCK_COLUMNS = 64;
-  config.HALF_BLOCK_ROWS = config.BLOCK_ROWS / 2;
-  config.HALF_BLOCK_COLUMNS = config.BLOCK_COLUMNS / 2;
-  config.TERRAIN_ROWS = 128;
-  config.TERRAIN_COLUMNS = 128;
-  config.HALF_TERRAIN_ROWS = config.TERRAIN_ROWS / 2;
-  config.HALF_TERRAIN_COLUMNS = config.TERRAIN_COLUMNS / 2;
-  config.MIN_STORY_HEIGHT = 1.2;
-  config.MAX_STORY_HEIGHT = 1.5;
-  config.MAX_BUILDING_MATERIALS = 50;
-
-  return config;
-})();
-
 CityTour.Coordinates = (function() {
   var coordinates = {};
 
