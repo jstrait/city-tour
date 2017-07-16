1987d80a2e2281fde21549005a998955534d6a9a
Joel Strait <joel.strait@gmail.com>
2017-07-16 16:02:53 -0700

    Extracting WorldGenerator function to its own source file
    Also converting it to an IIFE to be consistent with the other
    generator functions. This allows it to be used without having to
    create a new instance.

--- src/city.js
+++ src/city.js
@@ -86,76 +86,9 @@ CityTour.RenderView = function(container, scene) {
 };
 
 
-CityTour.WorldGenerator = function() {
-  var generate = function(config) {
-    var GENERATE_BUILDINGS = true;
-
-    var combinedStartTime = new Date();
-
-    var terrainConfig = {
-      heightJitter: config.terrain.heightJitter,
-      heightJitterDecay: config.terrain.heightJitterDecay,
-      river: (Math.random() < config.terrain.probabilityOfRiver),
-    };
-
-    var terrainStartTime = new Date();
-    var terrain = CityTour.TerrainGenerator.generate(CityTour.Config.TERRAIN_COLUMNS, CityTour.Config.TERRAIN_ROWS, terrainConfig);
-    var terrainEndTime = new Date();
-
-    var centerX = 0, centerZ = 0;
-    while(terrain.materialAtCoordinates(centerX, centerZ) != CityTour.Terrain.LAND) {
-      centerZ -= 1;
-    }
-
-    var roadConfig = {
-      centerMapX: centerX,
-      centerMapZ: centerZ,
-      safeFromDecayPercentage: config.roadNetwork.safeFromDecayPercentage,
-    };
-
-    var roadStartTime = new Date();
-    var roadNetwork = CityTour.RoadNetworkGenerator.generate(terrain, roadConfig);
-    var roadEndTime = new Date();
-
-    var zonedBlockConfig = {
-      percentageDistanceDecayBegins: config.zonedBlocks.percentageDistanceDecayBegins,
-      maxBuildingStories: config.zonedBlocks.maxBuildingStories,
-    };
-
-    var zonedBlocksStartTime = new Date();
-    var zonedBlocks = (GENERATE_BUILDINGS) ? CityTour.ZonedBlockGenerator.generate(terrain, roadNetwork, centerX, centerZ, zonedBlockConfig) : false;
-    var zonedBlocksEndTime = new Date();
-    var buildingsStartTime = new Date();
-    var buildings = (GENERATE_BUILDINGS) ? CityTour.BuildingsGenerator.generate(terrain, zonedBlocks) : false;
-    var buildingsEndTime = new Date();
-
-    var combinedEndTime = new Date();
-
-    console.log("Time to generate world data: " + (combinedEndTime - combinedStartTime) + "ms");
-    console.log("  Terrain:      " + (terrainEndTime - terrainStartTime) + "ms");
-    console.log("  Road Network: " + (roadEndTime - roadStartTime) + "ms");
-    console.log("  Lots:         " + (zonedBlocksEndTime - zonedBlocksStartTime) + "ms");
-    console.log("  Buildings:    " + (buildingsEndTime - buildingsStartTime) + "ms");
-
-    return {
-      terrain: terrain,
-      roadNetwork: roadNetwork,
-      buildings: buildings,
-      centerX: centerX,
-      centerZ: centerZ,
-    };
-  };
-
-  return {
-    generate: generate,
-  };
-};
-
-
 CityTour.City = function(container) {
   var scene, timer, animationManager;
   var renderView;
-  var worldGenerator = new CityTour.WorldGenerator();
 
   var detectWebGL = function() {
     if (!window.WebGLRenderingContext) {
@@ -188,7 +121,7 @@ CityTour.City = function(container) {
       },
     };
 
-    return worldGenerator.generate(config);
+    return CityTour.WorldGenerator.generate(config);
   };
 
   var init = function(onComplete) {
