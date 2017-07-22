49d7d246acde311c38f99de0e03e7062fa782beb
Joel Strait <joel.strait@gmail.com>
2017-07-22 14:24:01 -0700

    Fixing broken tests
    The RoadNetwork constructor and addEdge() signatures changed
    over time, but these test were never updated to reflect this
    until now.

--- spec/spec/road_network_spec.js
+++ spec/spec/road_network_spec.js
@@ -1,19 +1,60 @@
 "use strict";
 
 describe("CityTour.RoadNetwork", function() {
+  var LAND = CityTour.Terrain.LAND;
+  var TERRAIN_MESH = {
+    0: {
+      0: {material: LAND, height: 0.0 },
+      1: {material: LAND, height: 0.0 },
+      2: {material: LAND, height: 2.0 },
+      3: {material: LAND, height: 3.0 },
+      4: {material: LAND, height: 0.0 },
+    },
+    1: {
+      0: {material: LAND, height: 0.0 },
+      1: {material: LAND, height: 0.0 },
+      2: {material: LAND, height: 4.3 },
+      3: {material: LAND, height: 5.2 },
+      4: {material: LAND, height: 0.0 },
+    },
+    2: {
+      0: {material: LAND, height: 0.0 },
+      1: {material: LAND, height: 0.0 },
+      2: {material: LAND, height: 6.4 },
+      3: {material: LAND, height: 0.0 },
+      4: {material: LAND, height: 0.0 },
+    },
+    3: {
+      0: {material: LAND, height: 0.0 },
+      1: {material: LAND, height: 0.0 },
+      2: {material: LAND, height: 0.0 },
+      3: {material: LAND, height: 0.0 },
+      4: {material: LAND, height: 0.0 },
+    },
+    4: {
+      0: {material: LAND, height: 0.0 },
+      1: {material: LAND, height: 0.0 },
+      2: {material: LAND, height: 0.0 },
+      3: {material: LAND, height: 0.0 },
+      4: {material: LAND, height: 0.0 },
+    },
+  };
+
+  var terrain = new CityTour.Terrain(TERRAIN_MESH, 1);
+
   it(".hasIntersection", function() {
-    var roadNetwork = new CityTour.RoadNetwork();
+    var roadNetwork = new CityTour.RoadNetwork(terrain);
 
     expect(roadNetwork.hasIntersection(1, 2)).toBe(false);
     expect(roadNetwork.hasIntersection(3, 4)).toBe(false);
     expect(roadNetwork.hasIntersection(5, 6)).toBe(false);
 
-    roadNetwork.addEdge(1, 2, 3, 4);
+    roadNetwork.addEdge(1, 2, 3, 4, 0.0, CityTour.RoadNetwork.TERRAIN_SURFACE);
 
     expect(roadNetwork.hasIntersection(1, 2)).toBe(true);
     expect(roadNetwork.hasIntersection(3, 4)).toBe(true);
     expect(roadNetwork.hasIntersection(5, 6)).toBe(false);
-  
+
     expect(roadNetwork.hasIntersection(5.5, 6)).toBe(false);
     expect(roadNetwork.hasIntersection(5, 6.2)).toBe(false);
     expect(roadNetwork.hasIntersection(5.5, 6.2)).toBe(false);
@@ -21,7 +62,7 @@ describe("CityTour.RoadNetwork", function() {
   });
 
   it(".hasEdgeBetween", function() {
-    var roadNetwork = new CityTour.RoadNetwork();
+    var roadNetwork = new CityTour.RoadNetwork(terrain);
 
     expect(roadNetwork.hasEdgeBetween(1, 2, 3, 4)).toBe(false);
     expect(roadNetwork.hasEdgeBetween(3, 4, 1, 2)).toBe(false);
@@ -31,7 +72,7 @@ describe("CityTour.RoadNetwork", function() {
     expect(roadNetwork.hasEdgeBetween(0, 3, 3, 4)).toBe(false);
     expect(roadNetwork.hasEdgeBetween(1, 2, 4, 3)).toBe(false);
 
-    roadNetwork.addEdge(1, 2, 3, 4);
+    roadNetwork.addEdge(1, 2, 3, 4, 0.0, CityTour.RoadNetwork.TERRAIN_SURFACE);
 
     expect(roadNetwork.hasEdgeBetween(1, 2, 3, 4)).toBe(true);
     expect(roadNetwork.hasEdgeBetween(3, 4, 1, 2)).toBe(true);
@@ -41,7 +82,7 @@ describe("CityTour.RoadNetwork", function() {
     expect(roadNetwork.hasEdgeBetween(0, 3, 3, 4)).toBe(false);
     expect(roadNetwork.hasEdgeBetween(1, 2, 4, 3)).toBe(false);
 
-    roadNetwork.addEdge(1, 2, 0, 3);
+    roadNetwork.addEdge(1, 2, 0, 3, 0.0, CityTour.RoadNetwork.TERRAIN_SURFACE);
 
     expect(roadNetwork.hasEdgeBetween(1, 2, 3, 4)).toBe(true);
     expect(roadNetwork.hasEdgeBetween(3, 4, 1, 2)).toBe(true);
