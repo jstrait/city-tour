d56e8357109c879d63cc465c3a8077c624ba101f
Joel Strait <joel.strait@gmail.com>
2017-07-22 14:12:23 -0700

    Fixing incorrect test expectations
    I believe these tests started breaking when the street width config
    was changed in ad33217a0fa2a7ca1e0b72e076554670ebf36cbc. To fix
    the tests, the test expectations are being changed to reflect the
    current street width.

--- spec/spec/coordinates_spec.js
+++ spec/spec/coordinates_spec.js
@@ -7,15 +7,15 @@ describe("CityTour.Coordinates", function() {
     });
 
     it("returns the correct value for positive coordinates", function() {
-      expect(CityTour.Coordinates.mapXToSceneX(1.0)).toBe(11);
-      expect(CityTour.Coordinates.mapXToSceneX(2.1)).toBe(23.1);
-      expect(CityTour.Coordinates.mapXToSceneX(5)).toBe(55);
+      expect(CityTour.Coordinates.mapXToSceneX(1.0)).toBe(12);
+      expect(CityTour.Coordinates.mapXToSceneX(2.1)).toBeCloseTo(25.2);
+      expect(CityTour.Coordinates.mapXToSceneX(5)).toBe(60);
     });
 
     it("returns the correct value for negative coordinates", function() {
-      expect(CityTour.Coordinates.mapXToSceneX(-1.0)).toBe(-11);
-      expect(CityTour.Coordinates.mapXToSceneX(-2.1)).toBe(-23.1);
-      expect(CityTour.Coordinates.mapXToSceneX(-5)).toBe(-55);
+      expect(CityTour.Coordinates.mapXToSceneX(-1.0)).toBe(-12);
+      expect(CityTour.Coordinates.mapXToSceneX(-2.1)).toBeCloseTo(-25.2);
+      expect(CityTour.Coordinates.mapXToSceneX(-5)).toBe(-60);
     });
   });
 
@@ -25,15 +25,15 @@ describe("CityTour.Coordinates", function() {
     });
 
     it("returns the correct value for positive coordinates", function() {
-      expect(CityTour.Coordinates.mapZToSceneZ(1.0)).toBe(11);
-      expect(CityTour.Coordinates.mapZToSceneZ(2.1)).toBe(23.1);
-      expect(CityTour.Coordinates.mapZToSceneZ(5)).toBe(55);
+      expect(CityTour.Coordinates.mapZToSceneZ(1.0)).toBe(12);
+      expect(CityTour.Coordinates.mapZToSceneZ(2.1)).toBeCloseTo(25.2);
+      expect(CityTour.Coordinates.mapZToSceneZ(5)).toBe(60);
     });
 
     it("returns the correct value for negative coordinates", function() {
-      expect(CityTour.Coordinates.mapZToSceneZ(-1.0)).toBe(-11);
-      expect(CityTour.Coordinates.mapZToSceneZ(-2.1)).toBe(-23.1);
-      expect(CityTour.Coordinates.mapZToSceneZ(-5)).toBe(-55);
+      expect(CityTour.Coordinates.mapZToSceneZ(-1.0)).toBe(-12);
+      expect(CityTour.Coordinates.mapZToSceneZ(-2.1)).toBeCloseTo(-25.2);
+      expect(CityTour.Coordinates.mapZToSceneZ(-5)).toBe(-60);
     });
   });
 
@@ -43,15 +43,15 @@ describe("CityTour.Coordinates", function() {
     });
 
     it("returns the correct value for positive coordinates", function() {
-      expect(CityTour.Coordinates.sceneXToMapX(11)).toBe(1);
-      expect(CityTour.Coordinates.sceneXToMapX(23.1)).toBe(2.1);
-      expect(CityTour.Coordinates.sceneXToMapX(55)).toBe(5);
+      expect(CityTour.Coordinates.sceneXToMapX(12)).toBe(1.0);
+      expect(CityTour.Coordinates.sceneXToMapX(25.2)).toBe(2.1);
+      expect(CityTour.Coordinates.sceneXToMapX(60)).toBe(5.0);
     });
 
     it("returns the correct value for negative coordinates", function() {
-      expect(CityTour.Coordinates.sceneXToMapX(-11)).toBe(-1);
-      expect(CityTour.Coordinates.sceneXToMapX(-23.1)).toBe(-2.1);
-      expect(CityTour.Coordinates.sceneXToMapX(-55)).toBe(-5);
+      expect(CityTour.Coordinates.sceneXToMapX(-12)).toBe(-1.0);
+      expect(CityTour.Coordinates.sceneXToMapX(-25.2)).toBe(-2.1);
+      expect(CityTour.Coordinates.sceneXToMapX(-60)).toBe(-5.0);
     });
   });
 
@@ -61,15 +61,15 @@ describe("CityTour.Coordinates", function() {
     });
 
     it("returns the correct value for positive coordinates", function() {
-      expect(CityTour.Coordinates.sceneZToMapZ(11)).toBe(1);
-      expect(CityTour.Coordinates.sceneZToMapZ(23.1)).toBe(2.1);
-      expect(CityTour.Coordinates.sceneZToMapZ(55)).toBe(5);
+      expect(CityTour.Coordinates.sceneZToMapZ(12)).toBe(1.0);
+      expect(CityTour.Coordinates.sceneZToMapZ(25.2)).toBe(2.1);
+      expect(CityTour.Coordinates.sceneZToMapZ(60)).toBe(5.0);
     });
 
     it("returns the correct value for negative coordinates", function() {
-      expect(CityTour.Coordinates.sceneZToMapZ(-11)).toBe(-1);
-      expect(CityTour.Coordinates.sceneZToMapZ(-23.1)).toBe(-2.1);
-      expect(CityTour.Coordinates.sceneZToMapZ(-55)).toBe(-5);
+      expect(CityTour.Coordinates.sceneZToMapZ(-12)).toBe(-1.0);
+      expect(CityTour.Coordinates.sceneZToMapZ(-25.2)).toBe(-2.1);
+      expect(CityTour.Coordinates.sceneZToMapZ(-60)).toBe(-5.0);
     });
   });
 });
