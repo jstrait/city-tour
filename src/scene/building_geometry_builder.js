98a60f9c5d325a4bf4382b18ae45628bac17d1a4
Joel Strait <joel.strait@gmail.com>
2017-12-05 12:00:05 -0800

    Extracting anonymous function to bound function
    Makes the code a little easier to read. Also moving some variables
    to a more specific scope.

--- src/scene/building_geometry_builder.js
+++ src/scene/building_geometry_builder.js
@@ -39,11 +39,37 @@ CityTour.Scene.BuildingGeometryBuilder = function() {
 
     var mapX, mapZ, sceneX, sceneZ;
     var block;
-    var storyHeight, buildingHeight;
-    var materialIndex;
 
     var reusableBuildingMesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
-    var cylinderMesh;
+
+    var generateLotBuilding = function(lot) {
+      var storyHeight = Math.round(CityTour.Math.randomInRange(CityTour.Config.MIN_STORY_HEIGHT, CityTour.Config.MAX_STORY_HEIGHT));
+      var buildingHeight = storyHeight * lot.stories + (lot.ySurface - lot.yFloor);
+      var materialIndex = Math.floor(Math.random() * CityTour.Config.MAX_BUILDING_MATERIALS);
+      var cylinderMesh;
+
+      reusableBuildingMesh.scale.x = lot.dimensions.width * CityTour.Config.BLOCK_WIDTH;
+      reusableBuildingMesh.position.x = sceneX + (CityTour.Config.BLOCK_WIDTH * lot.dimensions.midpointX);
+
+      reusableBuildingMesh.scale.y = buildingHeight;
+      reusableBuildingMesh.position.y = (buildingHeight / 2) + lot.yFloor;
+
+      reusableBuildingMesh.scale.z = lot.dimensions.depth * CityTour.Config.BLOCK_DEPTH;
+      reusableBuildingMesh.position.z = sceneZ + (CityTour.Config.BLOCK_DEPTH * lot.dimensions.midpointZ);
+
+      reusableBuildingMesh.updateMatrix();
+
+      buildingGeometries[materialIndex].merge(reusableBuildingMesh.geometry, reusableBuildingMesh.matrix);
+
+      if (lot.roofStyle === 'antenna') {
+        cylinderMesh = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 10, 4));
+        cylinderMesh.position.x = sceneX + (CityTour.Config.BLOCK_WIDTH * lot.dimensions.midpointX);
+        cylinderMesh.position.y = lot.yFloor + buildingHeight + 5;
+        cylinderMesh.position.z = sceneZ + (CityTour.Config.BLOCK_DEPTH * lot.dimensions.midpointZ);
+        cylinderMesh.updateMatrix();
+        buildingGeometries[materialIndex].merge(cylinderMesh.geometry, cylinderMesh.matrix);
+      }
+    };
 
     for (mapX = roadNetwork.minColumn(); mapX < roadNetwork.maxColumn(); mapX++) {
       sceneX = CityTour.Coordinates.mapXToSceneX(mapX) + HALF_STREET_WIDTH;
@@ -52,34 +78,7 @@ CityTour.Scene.BuildingGeometryBuilder = function() {
         sceneZ = CityTour.Coordinates.mapZToSceneZ(mapZ) + HALF_STREET_DEPTH;
 
         block = buildings.blockAtCoordinates(mapX, mapZ);
-
-        block.forEach(function(lot) {
-          storyHeight = Math.round(CityTour.Math.randomInRange(CityTour.Config.MIN_STORY_HEIGHT, CityTour.Config.MAX_STORY_HEIGHT));
-          buildingHeight = storyHeight * lot.stories + (lot.ySurface - lot.yFloor);
-
-          reusableBuildingMesh.scale.x = lot.dimensions.width * CityTour.Config.BLOCK_WIDTH;
-          reusableBuildingMesh.position.x = sceneX + (CityTour.Config.BLOCK_WIDTH * lot.dimensions.midpointX);
-
-          reusableBuildingMesh.scale.y = buildingHeight;
-          reusableBuildingMesh.position.y = (buildingHeight / 2) + lot.yFloor;
-
-          reusableBuildingMesh.scale.z = lot.dimensions.depth * CityTour.Config.BLOCK_DEPTH;
-          reusableBuildingMesh.position.z = sceneZ + (CityTour.Config.BLOCK_DEPTH * lot.dimensions.midpointZ);
-
-          reusableBuildingMesh.updateMatrix();
-
-          materialIndex = Math.floor(Math.random() * CityTour.Config.MAX_BUILDING_MATERIALS);
-          buildingGeometries[materialIndex].merge(reusableBuildingMesh.geometry, reusableBuildingMesh.matrix);
-
-          if (lot.roofStyle === 'antenna') {
-            cylinderMesh = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 10, 4));
-            cylinderMesh.position.x = sceneX + (CityTour.Config.BLOCK_WIDTH * lot.dimensions.midpointX);
-            cylinderMesh.position.y = lot.yFloor + buildingHeight + 5;
-            cylinderMesh.position.z = sceneZ + (CityTour.Config.BLOCK_DEPTH * lot.dimensions.midpointZ);
-            cylinderMesh.updateMatrix();
-            buildingGeometries[materialIndex].merge(cylinderMesh.geometry, cylinderMesh.matrix);
-          }
-        });
+        block.forEach(generateLotBuilding);
       }
     }
   };
