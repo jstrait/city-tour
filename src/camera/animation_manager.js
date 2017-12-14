37cfb1cf89f7c7456b97942e365064cbda2516b5
Joel Strait <joel.strait@gmail.com>
2017-12-14 14:54:59 -0800

    Removing unused 'fly to bird's eye at city center' animation
    Was only used by the previous non-interactive version of the app.
    It allowed viewing a bird's eye view of the city, which was useful
    because otherwise it was only possible to view the city via a flythrough.
    Now that the normal interactive mode allows seeing the city from any
    position, and the old version of the app has been removed, there's no
    reason to keep this around any longer.

--- src/camera/animation_manager.js
+++ src/camera/animation_manager.js
@@ -3,13 +3,9 @@
 var CityTour = CityTour || {};
 
 CityTour.AnimationManager = function(terrain, roadNetwork, poleCamera, messageBroker) {
-  var DEBUG_UP_TO_BIRDS_EYE = 1;
-  var DEBUG_DOWN_TO_VEHICLE = 2;
-
   var enabled = false;
-  var debugDirection;
 
-  var vehicleController, debugAnimationController, directTargetAnimation;
+  var vehicleController, directTargetAnimation;
   var currentController;
 
   var syncCamera = function() {
@@ -61,58 +57,9 @@ CityTour.AnimationManager = function(terrain, roadNetwork, poleCamera, messageBr
           return;
         }
       }
-
-      if (debugAnimationController) {
-        if (debugDirection === DEBUG_DOWN_TO_VEHICLE) {
-          debugAnimationController.setTargetXPosition(vehicleController.positionX());
-          debugAnimationController.setTargetYPosition(vehicleController.positionY());
-          debugAnimationController.setTargetZPosition(vehicleController.positionZ());
-          debugAnimationController.setTargetXRotation(vehicleController.rotationX());
-          debugAnimationController.setTargetYRotation(vehicleController.rotationY());
-        }
-
-        debugAnimationController.tick();
-      }
     }
 
     syncCamera();
-
-    if (debugDirection === DEBUG_DOWN_TO_VEHICLE && debugAnimationController && debugAnimationController.finished()) {
-      debugDirection = undefined;
-      debugAnimationController = null;
-      currentController = vehicleController;
-    }
-  };
-
-  var toggleDebug = function() {
-    var initial = {
-      positionX: poleCamera.positionX(),
-      positionY: poleCamera.positionY(),
-      positionZ: poleCamera.positionZ(),
-      rotationX: poleCamera.rotationX(),
-      rotationY: poleCamera.rotationY(),
-    };
-
-    if (debugDirection === undefined) {
-      debugDirection = DEBUG_UP_TO_BIRDS_EYE;
-      debugAnimationController =
-        new CityTour.DebugAnimation(initial,
-                                    {positionX: 0.0, positionY: 900, positionZ: 0.0, rotationX: -(Math.PI / 2), rotationY: 0.0},
-                                    true);
-    }
-    else {
-      debugDirection = DEBUG_DOWN_TO_VEHICLE;
-      debugAnimationController =
-        new CityTour.DebugAnimation(initial,
-                                    {positionX: vehicleController.positionX(),
-                                     positionY: vehicleController.positionY(),
-                                     positionZ: vehicleController.positionZ(),
-                                     rotationX: vehicleController.rotationX(),
-                                     rotationY: vehicleController.rotationY()},
-                                    false);
-    }
-
-    currentController = debugAnimationController;
   };
 
 
@@ -120,6 +67,5 @@ CityTour.AnimationManager = function(terrain, roadNetwork, poleCamera, messageBr
     init: init,
     requestStop: requestStop,
     tick: tick,
-    toggleDebug: toggleDebug,
   };
 };
