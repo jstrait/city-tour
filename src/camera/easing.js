839f37c5df6d34b7bbd2d631f2cd7f804deb39f5
Joel Strait <joel.strait@gmail.com>
2017-12-19 15:35:48 -0800

    Extracting base object for easing generators, for simpler implementations
    This removes a lot of code duplicated in each individual easing function,
    and makes it easier to add new easing functions in the future. Now, new easing
    functions only have to implement the actual easing function, and not deal
    with details of how many frames, how to increment the x-value between each
    frame, etc. (With ES6 arrow functions this could be even more succinct).
    
    Since the code is small enough, combining all easing functions into the
    same source file.

---
+++ src/camera/easing.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var CityTour = CityTour || {};
+
+CityTour.BaseEasing = function(frameCount, easingFunc, minX, maxX) {
+  var frame = 0;
+  var deltaPerFrame;
+
+  var finished = function() {
+    return frame === frameCount;
+  };
+
+  var next = function() {
+    if (frame < frameCount) {
+      frame += 1;
+    }
+
+    return easingFunc(frame * deltaPerFrame);
+  };
+
+
+  if (frameCount === 0) {
+    frameCount = 1;
+  }
+  deltaPerFrame = (maxX - minX) / frameCount;
+
+  return {
+    next: next,
+    finished: finished,
+  };
+};
+
+
+CityTour.LinearEasing = function(frameCount) {
+  var easingFunc = function(x) {
+    return x;
+  };
+
+  return CityTour.BaseEasing(frameCount, easingFunc, 0.0, 1.0);
+};
+
+CityTour.SineEasing = function(frameCount, minX, maxX) {
+  var easingFunc = function(x) {
+    return Math.sin(x)
+  };
+
+  return CityTour.BaseEasing(frameCount, easingFunc, minX, maxX);
+};
+
+CityTour.CosineEasing = function(frameCount, minX, maxX) {
+  var easingFunc = function(x) {
+    return Math.cos(x)
+  };
+
+  return CityTour.BaseEasing(frameCount, easingFunc, minX, maxX);
+};
