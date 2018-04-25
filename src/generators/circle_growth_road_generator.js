5229ac9f0a309012c3b46cd477468e9233f0ebe4
Joel Strait <joel.strait@gmail.com>
2018-04-25 14:09:07 -0700

    Extracting object for generating road network for single neighborhood
    This is essentially the previous `RoadNetworkGenerator`, that is geared
    toward generating a single neighborhood (not the entire city). Extracting
    this allows the type of neighborhood road network that will be generated
    to be parameterized in the future.

---
+++ src/generators/circle_growth_road_generator.js
@@ -0,0 +1,110 @@
+"use strict";
+
+var CityTour = CityTour || {};
+
+CityTour.CircleGrowthRoadGenerator = (function() {
+  var MAX_STEEPNESS = Math.PI / 6;
+
+  var addNeighborhoodRoads = function(terrain, roadNetwork, neighborhoodCenterX, neighborhoodCenterZ, config) {
+    var MIN_MAP_X = Math.max(terrain.minMapX(), -(config.neighborhoods.columnCount / 2) + neighborhoodCenterX);
+    var MAX_MAP_X = Math.min(terrain.maxMapX(), (config.neighborhoods.columnCount / 2) + neighborhoodCenterX);
+    var MIN_MAP_Z = Math.max(terrain.minMapZ(), -(config.neighborhoods.rowCount / 2) + neighborhoodCenterZ);
+    var MAX_MAP_Z = Math.min(terrain.maxMapZ(), (config.neighborhoods.rowCount / 2) + neighborhoodCenterZ);
+
+    var DISTANCE_TO_NEIGHBORHOOD_BOUNDARY = Math.min(config.neighborhoods.columnCount / 2, config.neighborhoods.rowCount / 2);
+    var SAFE_FROM_DECAY_DISTANCE = config.safeFromDecayBlocks;
+
+    var probabilityOfBranching = function(mapX1, mapZ1, mapX2, mapZ2) {
+      // Guarantee roads along x and z axes
+      if (mapX1 === neighborhoodCenterX && mapX2 === neighborhoodCenterX && mapZ2 >= MIN_MAP_Z && mapZ2 <= MAX_MAP_Z) {
+        return 1.0;
+      }
+      else if (mapZ1 === neighborhoodCenterZ && mapZ2 === neighborhoodCenterZ && mapX2 >= MIN_MAP_X && mapX2 <= MAX_MAP_X) {
+        return 1.0;
+      }
+
+      var distanceFromCenter = CityTour.Math.distanceBetweenPoints(neighborhoodCenterX, neighborhoodCenterZ, mapX1, mapZ1);
+      var normalizedPercentageFromCenter;
+
+      if (distanceFromCenter <= SAFE_FROM_DECAY_DISTANCE) {
+        return 1.0;
+      }
+
+      normalizedPercentageFromCenter = (distanceFromCenter - SAFE_FROM_DECAY_DISTANCE) / (DISTANCE_TO_NEIGHBORHOOD_BOUNDARY - SAFE_FROM_DECAY_DISTANCE);
+      return (Math.pow(0.5, normalizedPercentageFromCenter) - 0.5) * 2;
+    };
+
+    var isTerrainTooSteep = function(terrain, mapX, mapZ, targetMapX, targetMapZ) {
+      var heightAtPoint1 = terrain.heightAtCoordinates(mapX, mapZ);
+      var heightAtPoint2 = terrain.heightAtCoordinates(targetMapX, targetMapZ);
+      var angle = Math.atan2((heightAtPoint1 - heightAtPoint2), CityTour.Config.BLOCK_DEPTH);
+
+      return Math.abs(angle) > MAX_STEEPNESS;
+    };
+
+    var shouldConnectIntersections = function(terrain, mapX1, mapZ1, mapX2, mapZ2) {
+      var edgeIsOnLand = terrain.waterHeightAtCoordinates(mapX1, mapZ1) === 0.0 &&
+                         terrain.waterHeightAtCoordinates(mapX2, mapZ2) === 0.0;
+
+      return edgeIsOnLand &&
+             (Math.random() < probabilityOfBranching(mapX1, mapZ1, mapX2, mapZ2)) &&
+             !isTerrainTooSteep(terrain, mapX1, mapZ1, mapX2, mapZ2);
+    };
+
+    var branchFromIntersection = function(terrain, roadNetwork, mapX, mapZ) {
+      connectIntersections(terrain, roadNetwork, mapX, mapZ, mapX - 1, mapZ);
+      connectIntersections(terrain, roadNetwork, mapX, mapZ, mapX, mapZ - 1);
+      connectIntersections(terrain, roadNetwork, mapX, mapZ, mapX + 1, mapZ);
+      connectIntersections(terrain, roadNetwork, mapX, mapZ, mapX, mapZ + 1);
+    };
+
+
+    var connectIntersections = function(terrain, roadNetwork, mapX, mapZ, targetMapX, targetMapZ) {
+      var bridgeAttributes;
+      var bridgeIntersectionX, bridgeIntersectionZ;
+      var targetIntersectionExists;
+
+      if (targetMapX < MIN_MAP_X || targetMapX > MAX_MAP_X || targetMapZ < MIN_MAP_Z || targetMapZ > MAX_MAP_Z) {
+        return;
+      }
+
+      if (terrain.waterHeightAtCoordinates(targetMapX, targetMapZ) > 0.0) {
+        bridgeAttributes = CityTour.BridgeGenerator.buildBridge(terrain, roadNetwork, mapX, mapZ, targetMapX, targetMapZ, config);
+
+        if (bridgeAttributes !== null) {
+          bridgeIntersectionX = mapX;
+          bridgeIntersectionZ = mapZ;
+          while (bridgeIntersectionX !== bridgeAttributes.endX || bridgeIntersectionZ !== bridgeAttributes.endZ) {
+            roadNetwork.addEdge(bridgeIntersectionX,
+                                bridgeIntersectionZ,
+                                bridgeIntersectionX + bridgeAttributes.xDelta,
+                                bridgeIntersectionZ + bridgeAttributes.zDelta,
+                                bridgeAttributes.roadDeckHeight,
+                                CityTour.RoadNetwork.BRIDGE_SURFACE);
+            bridgeIntersectionX += bridgeAttributes.xDelta;
+            bridgeIntersectionZ += bridgeAttributes.zDelta;
+          }
+
+          branchFromIntersection(terrain, roadNetwork, bridgeAttributes.endX, bridgeAttributes.endZ);
+        }
+      }
+      else {
+        if (shouldConnectIntersections(terrain, mapX, mapZ, targetMapX, targetMapZ)) {
+          targetIntersectionExists = roadNetwork.hasIntersection(targetMapX, targetMapZ);
+
+          roadNetwork.addEdge(mapX, mapZ, targetMapX, targetMapZ, 0.0, CityTour.RoadNetwork.TERRAIN_SURFACE);
+          if (!targetIntersectionExists) {
+            branchFromIntersection(terrain, roadNetwork, targetMapX, targetMapZ);
+          }
+        }
+      }
+    };
+
+    branchFromIntersection(terrain, roadNetwork, neighborhoodCenterX, neighborhoodCenterZ);
+  };
+
+
+  return {
+    addNeighborhoodRoads: addNeighborhoodRoads,
+  };
+})();
--- src/generators/neighborhood_road_network_generator.js
+++ src/generators/neighborhood_road_network_generator.js
@@ -3,17 +3,15 @@
 var CityTour = CityTour || {};
 
 CityTour.NeighborhoodRoadNetworkGenerator = (function() {
-  var MAX_STEEPNESS = Math.PI / 6;
-
   var buildRoadNetwork = function(terrain, neighborhoods, config) {
     var roadNetwork = new CityTour.RoadNetwork(terrain);
     var i;
 
-    buildNeighborhood(terrain, roadNetwork, neighborhoods[0].centerX, neighborhoods[0].centerZ, config);
+    CityTour.CircleGrowthRoadGenerator.addNeighborhoodRoads(terrain, roadNetwork, neighborhoods[0].centerX, neighborhoods[0].centerZ, config);
 
     for (i = 1; i < neighborhoods.length; i++) {
       buildRoadBetweenNeighborhoods(terrain, roadNetwork, neighborhoods[i].centerX, neighborhoods[i].centerZ, config.centerMapX, config.centerMapZ);
-      buildNeighborhood(terrain, roadNetwork, neighborhoods[i].centerX, neighborhoods[i].centerZ, config);
+      CityTour.CircleGrowthRoadGenerator.addNeighborhoodRoads(terrain, roadNetwork, neighborhoods[i].centerX, neighborhoods[i].centerZ, config);
     }
 
     return roadNetwork;
@@ -38,104 +36,6 @@ CityTour.NeighborhoodRoadNetworkGenerator = (function() {
       previousIntersectionX = shortestPath[i][0];
       previousIntersectionZ = shortestPath[i][1];
     }
-  };
-
-  var buildNeighborhood = function(terrain, roadNetwork, neighborhoodCenterX, neighborhoodCenterZ, config) {
-    var MIN_MAP_X = Math.max(terrain.minMapX(), -(config.neighborhoods.columnCount / 2) + neighborhoodCenterX);
-    var MAX_MAP_X = Math.min(terrain.maxMapX(), (config.neighborhoods.columnCount / 2) + neighborhoodCenterX);
-    var MIN_MAP_Z = Math.max(terrain.minMapZ(), -(config.neighborhoods.rowCount / 2) + neighborhoodCenterZ);
-    var MAX_MAP_Z = Math.min(terrain.maxMapZ(), (config.neighborhoods.rowCount / 2) + neighborhoodCenterZ);
-
-    var DISTANCE_TO_NEIGHBORHOOD_BOUNDARY = Math.min(config.neighborhoods.columnCount / 2, config.neighborhoods.rowCount / 2);
-    var SAFE_FROM_DECAY_DISTANCE = config.safeFromDecayBlocks;
-
-    var probabilityOfBranching = function(mapX1, mapZ1, mapX2, mapZ2) {
-      // Guarantee roads along x and z axes
-      if (mapX1 === neighborhoodCenterX && mapX2 === neighborhoodCenterX && mapZ2 >= MIN_MAP_Z && mapZ2 <= MAX_MAP_Z) {
-        return 1.0;
-      }
-      else if (mapZ1 === neighborhoodCenterZ && mapZ2 === neighborhoodCenterZ && mapX2 >= MIN_MAP_X && mapX2 <= MAX_MAP_X) {
-        return 1.0;
-      }
-
-      var distanceFromCenter = CityTour.Math.distanceBetweenPoints(neighborhoodCenterX, neighborhoodCenterZ, mapX1, mapZ1);
-      var normalizedPercentageFromCenter;
-
-      if (distanceFromCenter <= SAFE_FROM_DECAY_DISTANCE) {
-        return 1.0;
-      }
-
-      normalizedPercentageFromCenter = (distanceFromCenter - SAFE_FROM_DECAY_DISTANCE) / (DISTANCE_TO_NEIGHBORHOOD_BOUNDARY - SAFE_FROM_DECAY_DISTANCE);
-      return (Math.pow(0.5, normalizedPercentageFromCenter) - 0.5) * 2;
-    };
-
-    var isTerrainTooSteep = function(terrain, mapX, mapZ, targetMapX, targetMapZ) {
-      var heightAtPoint1 = terrain.heightAtCoordinates(mapX, mapZ);
-      var heightAtPoint2 = terrain.heightAtCoordinates(targetMapX, targetMapZ);
-      var angle = Math.atan2((heightAtPoint1 - heightAtPoint2), CityTour.Config.BLOCK_DEPTH);
-
-      return Math.abs(angle) > MAX_STEEPNESS;
-    };
-
-    var shouldConnectIntersections = function(terrain, mapX1, mapZ1, mapX2, mapZ2) {
-      var edgeIsOnLand = terrain.waterHeightAtCoordinates(mapX1, mapZ1) === 0.0 &&
-                         terrain.waterHeightAtCoordinates(mapX2, mapZ2) === 0.0;
-
-      return edgeIsOnLand &&
-             (Math.random() < probabilityOfBranching(mapX1, mapZ1, mapX2, mapZ2)) &&
-             !isTerrainTooSteep(terrain, mapX1, mapZ1, mapX2, mapZ2);
-    };
-
-    var branchFromIntersection = function(terrain, roadNetwork, mapX, mapZ) {
-      connectIntersections(terrain, roadNetwork, mapX, mapZ, mapX - 1, mapZ);
-      connectIntersections(terrain, roadNetwork, mapX, mapZ, mapX, mapZ - 1);
-      connectIntersections(terrain, roadNetwork, mapX, mapZ, mapX + 1, mapZ);
-      connectIntersections(terrain, roadNetwork, mapX, mapZ, mapX, mapZ + 1);
-    };
-
-
-    var connectIntersections = function(terrain, roadNetwork, mapX, mapZ, targetMapX, targetMapZ) {
-      var bridgeAttributes;
-      var bridgeIntersectionX, bridgeIntersectionZ;
-      var targetIntersectionExists;
-
-      if (targetMapX < MIN_MAP_X || targetMapX > MAX_MAP_X || targetMapZ < MIN_MAP_Z || targetMapZ > MAX_MAP_Z) {
-        return;
-      }
-
-      if (terrain.waterHeightAtCoordinates(targetMapX, targetMapZ) > 0.0) {
-        bridgeAttributes = CityTour.BridgeGenerator.buildBridge(terrain, roadNetwork, mapX, mapZ, targetMapX, targetMapZ, config);
-
-        if (bridgeAttributes !== null) {
-          bridgeIntersectionX = mapX;
-          bridgeIntersectionZ = mapZ;
-          while (bridgeIntersectionX !== bridgeAttributes.endX || bridgeIntersectionZ !== bridgeAttributes.endZ) {
-            roadNetwork.addEdge(bridgeIntersectionX,
-                                bridgeIntersectionZ,
-                                bridgeIntersectionX + bridgeAttributes.xDelta,
-                                bridgeIntersectionZ + bridgeAttributes.zDelta,
-                                bridgeAttributes.roadDeckHeight,
-                                CityTour.RoadNetwork.BRIDGE_SURFACE);
-            bridgeIntersectionX += bridgeAttributes.xDelta;
-            bridgeIntersectionZ += bridgeAttributes.zDelta;
-          }
-
-          branchFromIntersection(terrain, roadNetwork, bridgeAttributes.endX, bridgeAttributes.endZ);
-        }
-      }
-      else {
-        if (shouldConnectIntersections(terrain, mapX, mapZ, targetMapX, targetMapZ)) {
-          targetIntersectionExists = roadNetwork.hasIntersection(targetMapX, targetMapZ);
-
-          roadNetwork.addEdge(mapX, mapZ, targetMapX, targetMapZ, 0.0, CityTour.RoadNetwork.TERRAIN_SURFACE);
-          if (!targetIntersectionExists) {
-            branchFromIntersection(terrain, roadNetwork, targetMapX, targetMapZ);
-          }
-        }
-      }
-    };
-
-    branchFromIntersection(terrain, roadNetwork, neighborhoodCenterX, neighborhoodCenterZ);
   };
 
 
