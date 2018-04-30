1421abc0a804a9c87f80f45b9bcfdc709a686e48
Joel Strait <joel.strait@gmail.com>
2018-04-30 14:25:38 -0700

    Adding shape generators for sheer ridge, sloped ridge on terrain
    Similar to the "plateau" and "pyramid" shapes, these are intended
    to be used with erosion for more realistic looking results.
    
    The sloped ridge generator is not ideal, because the ends on the
    ridge result in a sheer drop off. This is because a "distance of
    point to line" formula is used to determine the terrain height,
    instead of a "distance of point to line segment" formula.

--- src/generators/terrain_shape_generator.js
+++ src/generators/terrain_shape_generator.js
@@ -44,9 +44,68 @@ CityTour.TerrainShapeGenerator = (function() {
     }
   };
 
+  var addLine = function(terrainCoordinates, startX, startZ, endX, endZ, thickness, height) {
+    var halfThickness = thickness / 2;
+    var boundingStartX = Math.max(0, startX - halfThickness);
+    var boundingStartZ = Math.max(0, startZ - halfThickness);
+    var boundingEndX = Math.min(terrainCoordinates.length - 1, endX + halfThickness);
+    var boundingEndZ = Math.min(terrainCoordinates[0].length - 1, endZ + halfThickness);
+    var numerator, denominator, distanceToLine;
+    var x, z;
+
+    denominator = CityTour.Math.distanceBetweenPoints(startX, startZ, endX, endZ);
+
+    for (x = boundingStartX; x <= boundingEndX; x++) {
+      for (z = boundingStartZ; z <= boundingEndZ; z++) {
+        distanceToLine = Math.random();
+
+        // https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+        numerator = Math.abs(((endZ - startZ) * x) - ((endX - startX) * z) + (endX * startZ) - (endZ * startX));
+        distanceToLine = numerator / denominator;
+
+        if (distanceToLine <= halfThickness) {
+          terrainCoordinates[x][z].landHeight += height;
+        }
+      }
+    }
+  };
+
+  var addRidge = function(terrainCoordinates, startX, startZ, endX, endZ, thickness, height) {
+    var halfThickness = thickness / 2;
+    var boundingStartX = Math.max(0, startX - halfThickness);
+    var boundingStartZ = Math.max(0, startZ - halfThickness);
+    var boundingEndX = Math.min(terrainCoordinates.length - 1, endX + halfThickness);
+    var boundingEndZ = Math.min(terrainCoordinates[0].length - 1, endZ + halfThickness);
+    var numerator, denominator, distanceToLine;
+    var pointHeight;
+    var x, z;
+
+    console.log("Coordinates: " + startX + ", " + startZ + " : " + endX + ", " + endZ);
+    console.log("Bounding:    " + boundingStartX + ", " + boundingStartZ + " : " + boundingEndX + ", " + boundingEndZ);
+
+    denominator = CityTour.Math.distanceBetweenPoints(startX, startZ, endX, endZ);
+
+    for (x = boundingStartX; x <= boundingEndX; x++) {
+      for (z = boundingStartZ; z <= boundingEndZ; z++) {
+        distanceToLine = Math.random();
+
+        // https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+        numerator = Math.abs(((endZ - startZ) * x) - ((endX - startX) * z) + (endX * startZ) - (endZ * startX));
+        distanceToLine = numerator / denominator;
+
+        if (distanceToLine <= halfThickness) {
+          pointHeight = (1.0 - (distanceToLine / halfThickness)) * height;
+          terrainCoordinates[x][z].landHeight += pointHeight;
+        }
+      }
+    }
+  };
+
 
   return {
     addPlateau: addPlateau,
     addPyramid: addPyramid,
+    addLine: addLine,
+    addRidge: addRidge,
   };
 })();
