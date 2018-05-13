92aed6bb136deb7069e5410c59b4da373a630121
Joel Strait <joel.strait@gmail.com>
2018-05-13 08:46:42 -0700

    Add marker for center of city coordinates, for debugging
    By default this marker is turned off.

--- src/app/scene_view.js
+++ src/app/scene_view.js
@@ -4,6 +4,8 @@ var CityTour = CityTour || {};
 
 
 CityTour.SceneView = function(containerEl, initialWorldData, messageBroker) {
+  var SHOW_MARKERS = false;
+
   var sceneBuilder = new CityTour.Scene.Builder();
   var scene = sceneBuilder.buildEmptyScene();
   var renderView = new CityTour.RenderView(containerEl, scene);
@@ -14,6 +16,7 @@ CityTour.SceneView = function(containerEl, initialWorldData, messageBroker) {
     var terrainStartTime, terrainEndTime;
     var roadStartTime, roadEndTime;
     var buildingsStartTime, buildingsEndTime;
+    var markersStartTime, markersEndTime;
 
     destroyPreviousMeshes();
 
@@ -31,20 +34,43 @@ CityTour.SceneView = function(containerEl, initialWorldData, messageBroker) {
     scene.add(sceneBuilder.buildBuildingMeshes(newWorldData.buildings, newWorldData.roadNetwork));
     buildingsEndTime = new Date();
 
+    markersStartTime = new Date();
+    if (SHOW_MARKERS) {
+      scene.add(buildMarkerMeshes(newWorldData));
+    }
+    markersEndTime = new Date();
+
     masterEndTime = new Date();
 
     console.log("Time to generate scene geometry: " + (masterEndTime - masterStartTime) + "ms");
     console.log("  Terrain:   " + (terrainEndTime - terrainStartTime) + "ms");
     console.log("  Roads:     " + (roadEndTime - roadStartTime) + "ms");
     console.log("  Buildings: " + (buildingsEndTime - buildingsStartTime) + "ms");
+    console.log("  Markers:   " + (markersEndTime - markersStartTime) + "ms");
 
     renderView.resize();
+  };
+
+  var buildMarkerMeshes = function(newWorldData) {
+    var group = new THREE.Group();
+    group.name = "markerMeshes";
+
+    var centerOfCityMarkerMesh = new THREE.Mesh(new THREE.BoxGeometry(5, 200, 5),
+                                                new THREE.MeshBasicMaterial({ color: 0xff00ff }));
+
+    centerOfCityMarkerMesh.position.x = newWorldData.centerX;
+    centerOfCityMarkerMesh.position.z = newWorldData.centerZ;
+
+    group.add(centerOfCityMarkerMesh);
+
+    return group;
   };
 
   var destroyPreviousMeshes = function() {
     var terrainMeshes = scene.getObjectByName("terrainMeshes");
     var roadNetworkMeshes = scene.getObjectByName("roadNetworkMeshes");
     var buildingMeshes = scene.getObjectByName("buildingMeshes");
+    var markerMeshes = scene.getObjectByName("markerMeshes");
 
     if (terrainMeshes !== undefined) {
       removeChildFromScene(terrainMeshes);
@@ -56,6 +82,10 @@ CityTour.SceneView = function(containerEl, initialWorldData, messageBroker) {
 
     if (buildingMeshes !== undefined) {
       removeChildFromScene(buildingMeshes);
+    }
+
+    if (markerMeshes !== undefined) {
+      removeChildFromScene(markerMeshes);
     }
   };
 
