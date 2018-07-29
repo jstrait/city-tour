36833371153c083df283571a54004736587067fa
Joel Strait <joel.strait@gmail.com>
2018-07-28 21:58:19 -0700

    More efficient way to avoid placing neighborhoods too closely
    Each terrain point now has the concept of a "close neighbor penalty".
    This penalty starts at 0 for every terrain point, meaning that there
    is no penalty. When a neighborhood is built, terrain points near the
    neighborhood center are set to have an infinite penalty, preventing
    a neighborhood from being built there.
    
    The benefit of this approach is that the distance to the closest
    neighborhood no longer needs to be calculated for every terrain point
    on each neighborhood placement iteration - only the points near the
    new neighborhood. Since the penalty is essentially binary, once the
    value is set to infinity is will always be enabled regardless of any
    other neighborhoods that get built.

--- src/generators/neighborhood_generator.js
+++ src/generators/neighborhood_generator.js
@@ -26,14 +26,14 @@ CityTour.NeighborhoodGenerator = (function() {
       scores[x] = [];
 
       for (z = minZ; z <= maxZ; z++) {
-        score = { flatness: Number.POSITIVE_INFINITY, centrality: Math.abs(x) + Math.abs(z) };
+        score = { flatness: Number.POSITIVE_INFINITY, centrality: Math.abs(x) + Math.abs(z), closeNeighborhoodPenalty: 0, };
         if (terrain.waterHeightAtCoordinates(x, z) === 0.0) {
           score.flatness = averageHeightDifferenceAroundPoint(terrain, x, z);
         }
 
         scores[x][z] = score;
       }
-    }
+   }
 
     return scores;
   };
@@ -58,23 +58,23 @@ CityTour.NeighborhoodGenerator = (function() {
     return totalHeightDeltas / pointCount;
   };
 
-  var closestNeighborhoodDistance = function(neighborhoods, x, z) {
-    var minDistanceToNeighborhood = Number.POSITIVE_INFINITY;
-    var distanceToClosestNeighborhood;
-    var i;
+  var setCloseNeighborhoodPenalties = function(neighborhoodCenterX, neighborhoodCenterZ, terrain, scores) {
+    var distanceToNeighborhoodCenter;
+    var x, z;
 
-    if (neighborhoods.length === 0) {
-      return 0;
-    }
+    var minX = Math.max(terrain.minMapX() + FLATNESS_WINDOW_WIDTH_MARGIN, neighborhoodCenterX - MIN_DISTANCE_BETWEEN_NEIGHBORHOODS);
+    var maxX = Math.min(terrain.maxMapX() - FLATNESS_WINDOW_WIDTH_MARGIN, neighborhoodCenterX + MIN_DISTANCE_BETWEEN_NEIGHBORHOODS);
+    var minZ = Math.max(terrain.minMapZ() + FLATNESS_WINDOW_DEPTH_MARGIN, neighborhoodCenterZ - MIN_DISTANCE_BETWEEN_NEIGHBORHOODS);
+    var maxZ = Math.min(terrain.maxMapZ() - FLATNESS_WINDOW_DEPTH_MARGIN, neighborhoodCenterZ + MIN_DISTANCE_BETWEEN_NEIGHBORHOODS);
 
-    for (i = 0; i < neighborhoods.length; i++) {
-      distanceToClosestNeighborhood = CityTour.Math.distanceBetweenPoints(x, z, neighborhoods[i].centerX, neighborhoods[i].centerZ);
-      if (distanceToClosestNeighborhood < minDistanceToNeighborhood) {
-        minDistanceToNeighborhood = distanceToClosestNeighborhood;
+    for (x = minX; x <= maxX; x++) {
+      for (z = minZ; z <= maxZ; z++) {
+        distanceToNeighborhoodCenter = CityTour.Math.distanceBetweenPoints(x, z, neighborhoodCenterX, neighborhoodCenterZ);
+        if (distanceToNeighborhoodCenter < MIN_DISTANCE_BETWEEN_NEIGHBORHOODS) {
+          scores[x][z].closeNeighborhoodPenalty = Number.POSITIVE_INFINITY;
+        }
       }
     }
-
-    return minDistanceToNeighborhood;
   };
 
   var bestNeighborhoodSite = function(terrain, scores, neighborhoods) {
@@ -92,13 +92,7 @@ CityTour.NeighborhoodGenerator = (function() {
     for (x = minX; x < maxX; x++) {
       for (z = minZ; z < maxZ; z++) {
         scoreComponents = scores[x][z];
-
-        distanceToClosestNeighborhood = closestNeighborhoodDistance(neighborhoods, x, z);
-        if (distanceToClosestNeighborhood < MIN_DISTANCE_BETWEEN_NEIGHBORHOODS) {
-          distanceToClosestNeighborhood = 1000;
-        }
-
-        score = scoreComponents.centrality + (scoreComponents.flatness * 10) + distanceToClosestNeighborhood;
+        score = scoreComponents.centrality + (scoreComponents.flatness * 10) + scoreComponents.closeNeighborhoodPenalty;
 
         if (score < bestSiteScore) {
           bestSiteScore = score;
@@ -115,11 +109,14 @@ CityTour.NeighborhoodGenerator = (function() {
     var scores = calculateScores(terrain);
     var neighborhoods = [];
     var neighborhoodCenter;
-    var i;
+    var distanceToClosestNeighborhood;
+    var i, x, z;
 
     for (i = 0; i < count; i++) {
       neighborhoodCenter = bestNeighborhoodSite(terrain, scores, neighborhoods);
       neighborhoods.push({ centerX: neighborhoodCenter.x, centerZ: neighborhoodCenter.z });
+
+      setCloseNeighborhoodPenalties(neighborhoodCenter.x, neighborhoodCenter.z, terrain, scores);
     }
 
     return neighborhoods;
