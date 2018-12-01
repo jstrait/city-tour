db3a7fc03ef51b97daaf2f864e3bab40ce672312
Joel Strait <joel.strait@gmail.com>
2018-12-01 10:21:30 -0800

    Adding comments to help with future understanding of the code

--- src/app/world_touch_collection.js
+++ src/app/world_touch_collection.js
@@ -2,6 +2,11 @@
 
 var CityTour = CityTour || {};
 
+/*
+  A collection of simultaneous mouse/touch locations, with helper functions useful for
+  implementing interactive gestures, such as calculating the distance between the touches,
+  angle between the touches, etc.
+*/
 CityTour.WorldTouchCollection = function(el, camera, screenTouches, terrain) {
   var worldTouches = [];
   var distanceInScreenPixels;
@@ -28,6 +33,9 @@ CityTour.WorldTouchCollection = function(el, camera, screenTouches, terrain) {
                                                                  worldTouches[1].screenPixelX(),
                                                                  worldTouches[1].screenPixelY());
 
+    // Simulate a touch in the middle of the actual touchpoints, so that we can determine the
+    // terrain-aware world position in that spot. This will be the target point that actions
+    // such as zooming or rotating are based around.
     phantomMidpointTouch = CityTour.WorldTouch(el, camera, (screenTouches[0].x + screenTouches[1].x) / 2, (screenTouches[0].y + screenTouches[1].y) / 2, terrain);
     worldMidpoint = new THREE.Vector3(phantomMidpointTouch.worldX(), phantomMidpointTouch.worldY(), phantomMidpointTouch.worldZ());
 
