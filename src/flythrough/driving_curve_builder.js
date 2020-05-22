2f411596d3652e001d3ba2acce34df7959e69497
Joel Strait <joel.strait@gmail.com>
2020-05-22 15:27:52 -0700

    Sharing single vector for identical points
    In a driving animation curve, the end point of one segment and the
    start point of the next are always identical. This commit allows
    a single `THREE.Vector3` to be used for both points. This makes
    the code a little easier to maintain by reducing duplication.
    Also, the reduction in the number of `THREE.Vector3` objects that
    need to be created is a minor optimization.

--- src/flythrough/driving_curve_builder.js
+++ src/flythrough/driving_curve_builder.js
@@ -79,7 +79,7 @@ let DrivingCurveBuilder = (function() {
 
       // Straight segment through intersection
       if (isStraightIntersectionSegmentRequired === true) {
-        lineStartVector = new THREE.Vector3(curvePositionX, roadNetwork.getRoadHeight(curvePositionX, curvePositionZ) + MINIMUM_HEIGHT_OFF_GROUND, curvePositionZ);
+        lineStartVector = lineEndVector;
 
         distanceFromMiddleIntersectionX = ((isFinalPathSegment === true) ? 0 : Config.HALF_STREET_WIDTH);
         distanceFromMiddleIntersectionZ = ((isFinalPathSegment === true) ? 0 : Config.HALF_STREET_DEPTH);
@@ -95,7 +95,7 @@ let DrivingCurveBuilder = (function() {
 
       // Curve to next straight segment
       if (isCurvedIntersectionSegmentRequired === true) {
-        lineStartVector = new THREE.Vector3(curvePositionX, roadNetwork.getRoadHeight(curvePositionX, curvePositionZ) + MINIMUM_HEIGHT_OFF_GROUND, curvePositionZ);
+        lineStartVector = lineEndVector;
         controlPointVector = new THREE.Vector3(middleX, roadNetwork.getRoadHeight(middleX, middleZ) + MINIMUM_HEIGHT_OFF_GROUND, middleZ);
 
         curvePositionX += ((CURVE_START_DISTANCE * segment1DirectionX) + (CURVE_START_DISTANCE * segment2DirectionX));
