413c83ad0b115e6ddbdc7a5d71e69d72873f4606
Joel Strait <joel.strait@gmail.com>
2020-05-27 11:36:32 -0700

    Various small code cleanups
    * Using "rotationX"/"rotationY" instead of "tilt"/"azimuth". Since
      the relevant fields refer to an absolute camera rotation, rather
      than a position relative to a center point, this seems more
      clear.
    * When statements dealing with X and Y fields appear consecutively
      the X fields are now placed first, instead of second, which seems
      like a more consistent ordering with the rest of the app.
    * Using "locked" terminology to describe the fields relevant to
      the angles when the camera is locked in a particular direction,
      which seems more clear than the previous terminology.

--- src/app/flythrough_gesture_processor.js
+++ src/app/flythrough_gesture_processor.js
@@ -14,14 +14,14 @@ var FlythroughGestureProcessor = function() {
       vehicleView.disableResetToCenterAnimation();
 
       if (previousTouches === undefined) {
-        vehicleView.setBaseAngles();
+        vehicleView.lockAngles();
       }
       else if (currentTouches.count() === 1 || currentTouches.count() === 2) {
         normalizedDragDistanceX = currentTouches.normalizedScreenMidpoint().x - previousTouches.normalizedScreenMidpoint().x;
         normalizedDragDistanceY = currentTouches.normalizedScreenMidpoint().y - previousTouches.normalizedScreenMidpoint().y;
 
-        vehicleView.setAzimuthAngleOffset(vehicleView.azimuthAngleOffset() - (normalizedDragDistanceX * (Math.PI / 2)));
-        vehicleView.setTiltAngleOffset(vehicleView.tiltAngleOffset() + (normalizedDragDistanceY * (Math.PI / 2)));
+        vehicleView.setLockedRotationOffsetX(vehicleView.lockedRotationOffsetX() + (normalizedDragDistanceY * (Math.PI / 2)));
+        vehicleView.setLockedRotationOffsetY(vehicleView.lockedRotationOffsetY() - (normalizedDragDistanceX * (Math.PI / 2)));
       }
     }
 
--- src/flythrough/vehicle_view.js
+++ src/flythrough/vehicle_view.js
@@ -5,84 +5,88 @@ import { SmoothStepEasing } from "./easing";
 import { MotionGenerator } from "./motion_generator";
 
 var VehicleView = function(vehicleController) {
-  var MIN_AZIMUTH_ANGLE_OFFSET = -Math.PI / 2;
-  var MAX_AZIMUTH_ANGLE_OFFSET = Math.PI / 2;
-  var MIN_TILTANGLE_OFFSET = -Math.PI / 6;
-  var MAX_TILTANGLE_OFFSET = Math.PI / 4;
+  var MIN_ROTATION_X_OFFSET = -Math.PI / 6;
+  var MAX_ROTATION_X_OFFSET = Math.PI / 4;
+  var MIN_ROTATION_Y_OFFSET = -Math.PI / 2;
+  var MAX_ROTATION_Y_OFFSET = Math.PI / 2;
   var RESET_TO_CENTER_ANIMATION_FRAME_COUNT = 40;
 
-  var azimuthAngleOffset = 0.0;
-  var tiltAngleOffset = 0.0;
-  var resetToCenterAzimuthAngleMotionGenerator, resetToCenterTiltAngleMotionGenerator;
+  var resetToCenterRotationXMotionGenerator;
+  var resetToCenterRotationYMotionGenerator;
 
-  var baseAzimuthAngle = undefined;
-  var baseTiltAngle = undefined;
-  var finalAzimuthAngle = undefined;
-  var finalTiltAngle = undefined;
+  var lockedRotationX;
+  var lockedRotationY;
+  var lockedRotationOffsetX;
+  var lockedRotationOffsetY;
+  var finalLockedRotationX;
+  var finalLockedRotationY;
+
   var rotationX = vehicleController.rotationX();
   var rotationY = vehicleController.rotationY();
 
-  var setBaseAngles = function() {
-    baseAzimuthAngle = vehicleController.rotationY();
-    baseTiltAngle = vehicleController.rotationX();
-    azimuthAngleOffset = 0.0;
-    tiltAngleOffset = 0.0;
+  var lockAngles = function() {
+    lockedRotationX = vehicleController.rotationX();
+    lockedRotationY = vehicleController.rotationY();
+    lockedRotationOffsetX = 0.0;
+    lockedRotationOffsetY = 0.0;
   };
 
-  var setAzimuthAngleOffset = function(newAzimuthAngleOffset) {
-    azimuthAngleOffset = CityTourMath.clamp(newAzimuthAngleOffset, MIN_AZIMUTH_ANGLE_OFFSET, MAX_AZIMUTH_ANGLE_OFFSET);
+  var setLockedRotationOffsetX = function(newLockedRotationOffsetX) {
+    lockedRotationOffsetX = CityTourMath.clamp(newLockedRotationOffsetX, MIN_ROTATION_X_OFFSET, MAX_ROTATION_X_OFFSET);
   };
 
-  var setTiltAngleOffset = function(newTiltAngleOffset) {
-    tiltAngleOffset = CityTourMath.clamp(newTiltAngleOffset, MIN_TILTANGLE_OFFSET, MAX_TILTANGLE_OFFSET);
+  var setLockedRotationOffsetY = function(newLockedRotationOffsetY) {
+    lockedRotationOffsetY = CityTourMath.clamp(newLockedRotationOffsetY, MIN_ROTATION_Y_OFFSET, MAX_ROTATION_Y_OFFSET);
   };
 
   var tick = function() {
-    if (resetToCenterAzimuthAngleMotionGenerator) {
-      rotationY = CityTourMath.lerp(finalAzimuthAngle, vehicleController.rotationY(), resetToCenterAzimuthAngleMotionGenerator.next());
-      if (resetToCenterAzimuthAngleMotionGenerator.finished()) {
-        finalAzimuthAngle = undefined;
-        resetToCenterAzimuthAngleMotionGenerator = undefined;
-      }
-    }
-    else if (baseAzimuthAngle === undefined) {
-      rotationY = vehicleController.rotationY();
-    }
-    else {
-      rotationY = baseAzimuthAngle + azimuthAngleOffset;
-    }
+    if (resetToCenterRotationXMotionGenerator !== undefined) {
+      rotationX = CityTourMath.lerp(finalLockedRotationX, vehicleController.rotationX(), resetToCenterRotationXMotionGenerator.next());
 
-    if (resetToCenterTiltAngleMotionGenerator) {
-      rotationX = CityTourMath.lerp(finalTiltAngle, vehicleController.rotationX(), resetToCenterTiltAngleMotionGenerator.next());
-      if (resetToCenterTiltAngleMotionGenerator.finished()) {
-        finalTiltAngle = undefined;
-        resetToCenterTiltAngleMotionGenerator = undefined;
+      if (resetToCenterRotationXMotionGenerator.finished()) {
+        finalLockedRotationX = undefined;
+        resetToCenterRotationXMotionGenerator = undefined;
       }
     }
-    else if (baseTiltAngle === undefined) {
+    else if (lockedRotationX === undefined) {
       rotationX = vehicleController.rotationX();
     }
     else {
-      rotationX = baseTiltAngle + tiltAngleOffset;
+      rotationX = lockedRotationX + lockedRotationOffsetX;
+    }
+
+    if (resetToCenterRotationYMotionGenerator !== undefined) {
+      rotationY = CityTourMath.lerp(finalLockedRotationY, vehicleController.rotationY(), resetToCenterRotationYMotionGenerator.next());
+
+      if (resetToCenterRotationYMotionGenerator.finished()) {
+        finalLockedRotationY = undefined;
+        resetToCenterRotationYMotionGenerator = undefined;
+      }
+    }
+    else if (lockedRotationY === undefined) {
+      rotationY = vehicleController.rotationY();
+    }
+    else {
+      rotationY = lockedRotationY + lockedRotationOffsetY;
     }
   };
 
   var enableResetToCenterAnimation = function() {
-    var resetToCenterAzimuthAngleEasing = SmoothStepEasing(RESET_TO_CENTER_ANIMATION_FRAME_COUNT);
-    var resetToCenterTiltAngleEasing = SmoothStepEasing(RESET_TO_CENTER_ANIMATION_FRAME_COUNT);
+    var resetToCenterRotationXEasing = SmoothStepEasing(RESET_TO_CENTER_ANIMATION_FRAME_COUNT);
+    var resetToCenterRotationYEasing = SmoothStepEasing(RESET_TO_CENTER_ANIMATION_FRAME_COUNT);
 
-    finalAzimuthAngle = baseAzimuthAngle + azimuthAngleOffset;
-    finalTiltAngle = baseTiltAngle + tiltAngleOffset;
-    baseAzimuthAngle = undefined;
-    baseTiltAngle = undefined;
+    finalLockedRotationX = lockedRotationX + lockedRotationOffsetX;
+    finalLockedRotationY = lockedRotationY + lockedRotationOffsetY;
+    lockedRotationX = undefined;
+    lockedRotationY = undefined;
 
-    resetToCenterAzimuthAngleMotionGenerator = MotionGenerator(0.0, 1.0, resetToCenterAzimuthAngleEasing);
-    resetToCenterTiltAngleMotionGenerator = MotionGenerator(0.0, 1.0, resetToCenterTiltAngleEasing);
+    resetToCenterRotationXMotionGenerator = MotionGenerator(0.0, 1.0, resetToCenterRotationXEasing);
+    resetToCenterRotationYMotionGenerator = MotionGenerator(0.0, 1.0, resetToCenterRotationYEasing);
   };
 
   var disableResetToCenterAnimation = function() {
-    resetToCenterAzimuthAngleMotionGenerator = undefined;
-    resetToCenterTiltAngleMotionGenerator = undefined;
+    resetToCenterRotationXMotionGenerator = undefined;
+    resetToCenterRotationYMotionGenerator = undefined;
   };
 
   return {
@@ -91,11 +95,11 @@ var VehicleView = function(vehicleController) {
     positionZ: function() { return vehicleController.positionZ(); },
     rotationX: function() { return rotationX; },
     rotationY: function() { return rotationY; },
-    azimuthAngleOffset: function() { return azimuthAngleOffset; },
-    tiltAngleOffset: function() { return tiltAngleOffset; },
-    setBaseAngles: setBaseAngles,
-    setAzimuthAngleOffset: setAzimuthAngleOffset,
-    setTiltAngleOffset: setTiltAngleOffset,
+    lockedRotationOffsetX: function() { return lockedRotationOffsetX; },
+    lockedRotationOffsetY: function() { return lockedRotationOffsetY; },
+    lockAngles: lockAngles,
+    setLockedRotationOffsetX: setLockedRotationOffsetX,
+    setLockedRotationOffsetY: setLockedRotationOffsetY,
     tick: tick,
     enableResetToCenterAnimation: enableResetToCenterAnimation,
     disableResetToCenterAnimation: disableResetToCenterAnimation,
