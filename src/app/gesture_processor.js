83aee3d1e8bcdd3cb088850b07c34a660448e864
Joel Strait <joel.strait@gmail.com>
2020-05-28 21:38:02 -0700

    Better panning speed
    Previously, when the camera was tilted to be facing close to
    straight at the horizon, panning the world was way too slow,
    especially if you were zoomed closed to the terrain.
    
    Now, panning when the camera is tilted close to facing the horizon
    is much faster, which in practice makes it much more usable and
    pleasant.
    
    This conceptually uses the same "always pan as if you were looking
    straight down" logic as before (although implemented in a more
    clear way). The reason the panning speed is faster is that it
    bases the "looking down" position on where you would be if you
    performed a normal tilt gesture (which will raise you higher into
    the sky), rather than the actual camera position. The higher the
    vantage point, the faster the pan.
    
    One thing to note is that the panning speed can change while
    panning around, depending on what terrain is at the center of the
    window at any given moment. (I.e., what point in the world will be
    used as the "center of tilt"). This effect is more pronounced the
    more the camera is tilted to look straight at the horizon. This
    seems a little weird and ideally should be fixed in the future,
    but for now this still seems like an improvement over before.

--- src/app/gesture_processor.js
+++ src/app/gesture_processor.js
@@ -221,50 +221,24 @@ var GestureProcessor = function(sceneView, mapCamera, terrain) {
     var normalizedScreenDragDistance = new THREE.Vector3(currentTouches.normalizedScreenMidpoint().x - previousTouches.normalizedScreenMidpoint().x,
                                                          currentTouches.normalizedScreenMidpoint().y - previousTouches.normalizedScreenMidpoint().y,
                                                          0.0);
+    var tiltAngleDelta = mapCamera.minTiltAngle() - mapCamera.tiltAngle();
+    var worldDragStart;
+    var worldDragEnd;
+    var worldDragDistance;
 
-    var worldDragStart = new THREE.Vector3(mapCamera.positionX(), 0.0, mapCamera.positionZ());
-    var worldDragEnd = screenCoordinateToWorldCoordinateStraightDown(normalizedScreenDragDistance);
-    var worldDragDistance = new THREE.Vector3(worldDragEnd.x - worldDragStart.x,
-                                              worldDragEnd.y - worldDragStart.y,
-                                              worldDragEnd.z - worldDragStart.z);
+    mapCamera.setCenterOfTilt(WorldTouch(sceneView.camera(), SCREEN_CENTER, terrain).worldPosition());
+    mapCamera.tiltCamera(tiltAngleDelta);
+
+    worldDragStart = new THREE.Vector3(mapCamera.positionX(), 0.0, mapCamera.positionZ());
+    worldDragEnd = WorldTouch(sceneView.camera(), normalizedScreenDragDistance, terrain).worldPosition();
+    worldDragDistance = new THREE.Vector3(worldDragEnd.x - worldDragStart.x,
+                                          worldDragEnd.y - worldDragStart.y,
+                                          worldDragEnd.z - worldDragStart.z);
+
+    mapCamera.tiltCamera(-tiltAngleDelta);
+    mapCamera.setCenterOfTilt(undefined);
 
     mapCamera.pan(worldDragDistance.x, worldDragDistance.z);
-  };
-
-  // Adapted from https://stackoverflow.com/questions/13055214/mouse-canvas-x-y-to-three-js-world-x-y-z#13091694
-  //
-  // Instead of using the camera's current position to unproject from the screen coordinate to the world coordinate,
-  // this always uses the equivalent of a camera looking straight down on the center of orbit, from the zoom distance away.
-  //
-  // The reason for doing this is so that the rate of movement is uniform regardless of the camera's tilt angle.
-  // I.e., at the same zoom level, moving your mouse/finger 10 pixels on the screen will result in the same amount
-  // of world movement if the camera is looking straight down, or straight forward. It also means that the location on
-  // the screen (i.e. top-left vs. center vs. bottom-right) won't affect the amount of world movement when the camera is
-  // tilted at an angle other than straight down.
-  var screenCoordinateToWorldCoordinateStraightDown = function(normalizedScreenVector) {
-    var straightDownEuler, straightDownPosition, straightDownQuaternion, straightDownScale, straightDownMatrix;
-    var matrix;
-    var direction, distanceToXZPlane, worldPosition;
-    var camera = sceneView.camera();
-
-    // Similar camera world matrix from a "looking straight down on center of orbit" position/rotation
-    straightDownEuler = new THREE.Euler(-HALF_PI, camera.rotation.y, 0.0, 'YXZ');
-    straightDownPosition = new THREE.Vector3(mapCamera.positionX(), mapCamera.positionY(), mapCamera.positionZ());
-    straightDownQuaternion = new THREE.Quaternion();
-    straightDownQuaternion.setFromEuler(straightDownEuler);
-    straightDownScale = camera.scale.clone();
-    straightDownMatrix = new THREE.Matrix4().compose(straightDownPosition, straightDownQuaternion, straightDownScale);
-
-    // Unproject from the simulated camera position
-    matrix = new THREE.Matrix4();
-    matrix.multiplyMatrices(straightDownMatrix, matrix.getInverse(camera.projectionMatrix));
-    normalizedScreenVector.applyMatrix4(matrix);
-
-    direction = normalizedScreenVector.sub(straightDownPosition).normalize();
-    distanceToXZPlane = -(straightDownPosition.y / direction.y);
-    worldPosition = straightDownPosition.clone().add(direction.multiplyScalar(distanceToXZPlane));
-
-    return worldPosition;
   };
 
   var setTerrain = function(newTerrain) {
