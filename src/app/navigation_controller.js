cef0bc81455990653f877edc1a4f99e58487728b
Joel Strait <joel.strait@gmail.com>
2020-06-22 23:19:39 -0700

    Preventing text selection while holding zoom buttons
    Text selection is prevented on the zoom in/zoom out buttons, to
    prevent their text from being selected during a long press on iOS.
    However, as of iOS 13, this causes a long press to select the
    closest adjacent text instead (which happens to be the down arrow
    label of the tilt angle slider), unless this is stopped by a
    `preventDefault()` call.

--- src/app/navigation_controller.js
+++ src/app/navigation_controller.js
@@ -78,10 +78,26 @@ var NavigationController = function(sceneView, mapCamera, terrain, timerLoop, me
     mapCamera.setIsVelocityEnabled(false);
   };
 
+  var startZoomInTouch = function(e) {
+    startZoomIn(e);
+
+    // Prevent text in adjacent elements from being selected
+    // due to a long press gesture.
+    e.preventDefault();
+  };
+
   var startZoomOut = function(e) {
     setTargetOfAction(e);
     timerLoop.setZoomAmount(-ZOOM_DELTA_PERCENTAGE);
     mapCamera.setIsVelocityEnabled(false);
+  };
+
+  var startZoomOutTouch = function(e) {
+    startZoomOut(e);
+
+    // Prevent text in adjacent elements from being selected
+    // due to a long press gesture.
+    e.preventDefault();
   };
 
   var stopZoom = function(e) {
@@ -120,12 +136,12 @@ var NavigationController = function(sceneView, mapCamera, terrain, timerLoop, me
   zoomInButton.addEventListener("mousedown", startZoomIn, false);
   zoomInButton.addEventListener("mouseup", stopZoom, false);
   zoomInButton.addEventListener("mouseout", stopZoom, false);
-  zoomInButton.addEventListener("touchstart", startZoomIn, false);
+  zoomInButton.addEventListener("touchstart", startZoomInTouch, false);
   zoomInButton.addEventListener("touchend", stopZoom, false);
   zoomOutButton.addEventListener("mousedown", startZoomOut, false);
   zoomOutButton.addEventListener("mouseup", stopZoom, false);
   zoomOutButton.addEventListener("mouseout", stopZoom, false);
-  zoomOutButton.addEventListener("touchstart", startZoomOut, false);
+  zoomOutButton.addEventListener("touchstart", startZoomOutTouch, false);
   zoomOutButton.addEventListener("touchend", stopZoom, false);
   flythroughToggle.addEventListener("click", toggleFlythrough, false);
 
