jstrait/city-tour

commit 26e9ecfed4960273be2819b3c621d30ac7c7ae9a

Preventing roads and bridges on terrain edges
This is a follow on from 56ba22c05908998d44110119cdb6ef5a961ba791,
which prevented roads inside neighborhoods from being directly on
the terrain edges. This new commit also prevents roads (including
bridges) that connect neighborhoods from being on the terrain
edges as well.

As noted in the original commit, roads on the edge of the terrain
look weird because half of the road surface hangs off of the
terrain into the air.

This commit introduces fields to `RoadNetwork` that indicate the
bounding box that all roads must be inside. This allows
centralizing the relevant logic, removing the need to re-implement
it each place it is needed.

This also requires updating some test fixture data to compensate
for some roads in tests that previously were on the edges of the
terrain.

src/terrain_candidate_road_network.js
@@ -17,7 +17,7 @@ var TerrainCandidateRoadNetwork = function(terrain, roadNetwork, maxRoadAngle) {
   };
 
   var hasIntersection = function(x, z) {
-    return terrain.isPointInBounds(x, z);
+    return roadNetwork.isPointInAllowedBounds(x, z);
   };
 
   var edgesFrom = function(x, z) {

src/generators/bridge_generator.js
@@ -42,7 +42,7 @@ var BridgeGenerator = (function() {
       bridgeEndZ += zDelta;
       bridgeLength += 1;
 
-      if (!terrain.isPointInBounds(bridgeEndX, bridgeEndZ)) {
+      if (!roadNetwork.isPointInAllowedBounds(bridgeEndX, bridgeEndZ)) {
         return;
       }
     }

src/generators/circle_growth_road_generator.js
@@ -6,10 +6,10 @@ import { RoadNetwork } from "./../road_network";
 
 var CircleGrowthRoadGenerator = (function() {
   var addNeighborhoodRoads = function(terrain, roadNetwork, neighborhoodCenterX, neighborhoodCenterZ, config) {
-    var MIN_X = Math.max(terrain.minX() + 1, -(config.neighborhoods.columnCount / 2) + neighborhoodCenterX);
-    var MAX_X = Math.min(terrain.maxX() - 1, (config.neighborhoods.columnCount / 2) + neighborhoodCenterX);
-    var MIN_Z = Math.max(terrain.minZ() + 1, -(config.neighborhoods.rowCount / 2) + neighborhoodCenterZ);
-    var MAX_Z = Math.min(terrain.maxZ() - 1, (config.neighborhoods.rowCount / 2) + neighborhoodCenterZ);
+    var MIN_X = Math.max(roadNetwork.minAllowedX(), -(config.neighborhoods.columnCount / 2) + neighborhoodCenterX);
+    var MAX_X = Math.min(roadNetwork.maxAllowedX(), (config.neighborhoods.columnCount / 2) + neighborhoodCenterX);
+    var MIN_Z = Math.max(roadNetwork.minAllowedZ(), -(config.neighborhoods.rowCount / 2) + neighborhoodCenterZ);
+    var MAX_Z = Math.min(roadNetwork.maxAllowedZ(), (config.neighborhoods.rowCount / 2) + neighborhoodCenterZ);
 
     var DISTANCE_TO_NEIGHBORHOOD_BOUNDARY = Math.min(config.neighborhoods.columnCount / 2, config.neighborhoods.rowCount / 2);
     var SAFE_FROM_DECAY_DISTANCE = config.safeFromDecayBlocks;

src/road_network.js
@@ -77,6 +77,13 @@ var RoadNetwork = function(terrain) {
 
 
   var minColumn = Number.POSITIVE_INFINITY, maxColumn = Number.NEGATIVE_INFINITY, minRow = Number.POSITIVE_INFINITY, maxRow = Number.NEGATIVE_INFINITY;
+
+  // Don't allow roads on the terrain edges
+  var minAllowedX = terrain.minX() + 1;
+  var maxAllowedX = terrain.maxX() - 1;
+  var minAllowedZ = terrain.minZ() + 1;
+  var maxAllowedZ = terrain.maxZ() - 1;
+
   var intersections = [];
   for (var x = Math.ceil(terrain.minX()); x <= Math.floor(terrain.maxX()); x++) {
     intersections[x] = [];
@@ -165,6 +172,13 @@ var RoadNetwork = function(terrain) {
     var intersectionHeight, intersectionSurfaceType;
     var edge = { distance: distance, surfaceType: surfaceType };
 
+    if (x1 < minAllowedX || x1 > maxAllowedX || z1 < minAllowedZ || z1 > maxAllowedZ) {
+      throw new Error(`Road coordinates are out of allowed bounds: {${x1}, ${z1}}`);
+    }
+    if (x2 < minAllowedX || x2 > maxAllowedX || z2 < minAllowedZ || z2 > maxAllowedZ) {
+      throw new Error(`Road coordinates are out of allowed bounds: {${x2}, ${z2}}`);
+    }
+
     if (intersection1 === undefined) {
       intersectionHeight = (terrain.waterHeightAt(x1, z1) === 0.0) ? terrain.heightAt(x1, z1) : nonTerrainHeight;
       intersectionSurfaceType = (terrain.waterHeightAt(x1, z1) === 0.0) ? RoadNetwork.TERRAIN_SURFACE : RoadNetwork.BRIDGE_SURFACE;
@@ -227,6 +241,10 @@ var RoadNetwork = function(terrain) {
     return (intersection === undefined) ? undefined : intersection.edgesFrom();
   };
 
+  var isPointInAllowedBounds = function(x, z) {
+    return x >= minAllowedX && x <= maxAllowedX && z >= minAllowedZ && z <= maxAllowedZ;
+  };
+
 
   return {
     hasIntersection: hasIntersection,
@@ -241,6 +259,11 @@ var RoadNetwork = function(terrain) {
     maxColumn: function() { return maxColumn; },
     minRow: function() { return minRow; },
     maxRow: function() { return maxRow; },
+    minAllowedX: function() { return minAllowedX; },
+    maxAllowedX: function() { return maxAllowedX; },
+    minAllowedZ: function() { return minAllowedZ; },
+    maxAllowedZ: function() { return maxAllowedZ; },
+    isPointInAllowedBounds: isPointInAllowedBounds,
   };
 };
 

test/flythrough/aerial_navigator.test.js
@@ -12,11 +12,6 @@ describe("AerialNavigator", function() {
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
-    ],
-    [
-      {landHeight: 0.0, waterHeight: 0.0},
-      {landHeight: 0.0, waterHeight: 0.0},
-      {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
     ],
@@ -26,6 +21,17 @@ describe("AerialNavigator", function() {
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+    ],
+    [
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
     ],
     [
       {landHeight: 0.0, waterHeight: 0.0},
@@ -33,8 +39,30 @@ describe("AerialNavigator", function() {
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
     ],
     [
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+    ],
+    [
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+    ],
+    [
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},

test/path_finder.test.js
@@ -14,6 +14,8 @@ describe("PathFinder", function() {
     [ FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, ],
     [ FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, ],
     [ FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, ],
+    [ FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, ],
+    [ FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, FLAT_TERRAIN_POINT, ],
   ];
 
   var terrain = Terrain(terrainMesh, 1);

test/road_network.test.js
@@ -8,23 +8,47 @@ describe("RoadNetwork", function() {
     [
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
-      {landHeight: 2.0, waterHeight: 0.0},
-      {landHeight: 3.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
     ],
     [
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 2.0, waterHeight: 0.0},
+      {landHeight: 3.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+    ],
+    [
+      {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 4.3, waterHeight: 0.0},
       {landHeight: 5.2, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
     ],
     [
+      {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 6.4, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+    ],
+    [
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
     ],
     [
       {landHeight: 0.0, waterHeight: 0.0},
@@ -32,8 +56,12 @@ describe("RoadNetwork", function() {
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
     ],
     [
+      {landHeight: 0.0, waterHeight: 0.0},
+      {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
       {landHeight: 0.0, waterHeight: 0.0},
@@ -187,6 +215,51 @@ describe("RoadNetwork", function() {
     expect(roadNetwork.edgeBetween(0, 0, 1, 0)).toEqual({ distance: 1.0, surfaceType: RoadNetwork.TERRAIN_SURFACE });
 
     expect(roadNetwork.edgeBetween(1, 0, 2, 0)).toBe(undefined);
+  });
+
+
+  describe(".addEdge", function() {
+    var roadNetwork = new RoadNetwork(terrain);
+
+    it("allows adding an edge in a valid location", function() {
+      expect(roadNetwork.hasEdgeBetween(0, 0, 1, 0)).toBe(false);
+      expect(roadNetwork.getRoadHeight(0, 0)).toBe(undefined);
+      expect(roadNetwork.getRoadHeight(1, 0)).toBe(undefined);
+      expect(roadNetwork.getIntersectionSurfaceType(0, 0)).toBe(false);
+      expect(roadNetwork.getIntersectionSurfaceType(1, 0)).toBe(false);
+
+      roadNetwork.addEdge(0, 0, 1, 0, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE);
+
+      expect(roadNetwork.hasEdgeBetween(0, 0, 1, 0)).toBe(true);
+      expect(roadNetwork.getRoadHeight(0, 0)).toBe(6.4);
+      expect(roadNetwork.getRoadHeight(1, 0)).toBe(0.0);
+      expect(roadNetwork.getIntersectionSurfaceType(0, 0)).toBe(RoadNetwork.TERRAIN_SURFACE);
+      expect(roadNetwork.getIntersectionSurfaceType(1, 0)).toBe(RoadNetwork.TERRAIN_SURFACE);
+    });
+
+    it("allows adding an edge up to the valid bounds", function() {
+      roadNetwork.addEdge(-1, 0, -2, 0, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE);
+      roadNetwork.addEdge(1, 0, 2, 0, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE);
+      roadNetwork.addEdge(0, -1, 0, -2, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE);
+      roadNetwork.addEdge(0, 1, 0, 2, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE);
+
+      expect(roadNetwork.hasEdgeBetween(-1, 0, -2, 0)).toBe(true);
+      expect(roadNetwork.hasEdgeBetween(1, 0, 2, 0)).toBe(true);
+      expect(roadNetwork.hasEdgeBetween(0, -1, 0, -2)).toBe(true);
+      expect(roadNetwork.hasEdgeBetween(0, 1, 0, 2)).toBe(true);
+    });
+
+    it("raises an error if adding an edge outside the allowed bounds", function() {
+      expect(function() { roadNetwork.addEdge(-2, 0, -3, 0, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE) }).toThrowError(Error);
+      expect(function() { roadNetwork.addEdge(2, 0, 3, 0, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE) }).toThrowError(Error);
+      expect(function() { roadNetwork.addEdge(0, -2, 0, -3, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE) }).toThrowError(Error);
+      expect(function() { roadNetwork.addEdge(0, 2, 0, 3, 0.0, 1.0, RoadNetwork.TERRAIN_SURFACE) }).toThrowError(Error);
+
+      expect(roadNetwork.hasEdgeBetween(-2, 0, -3, 0)).toBe(false);
+      expect(roadNetwork.hasEdgeBetween(2, 0, 3, 0)).toBe(false);
+      expect(roadNetwork.hasEdgeBetween(0, -2, 0, -3)).toBe(false);
+      expect(roadNetwork.hasEdgeBetween(0, 2, 0, 3)).toBe(false);
+    });
   });
 
 

commit 7f5f22bae75f8cb2e111403e1ceb6c2ee7219686

Preventing error when generating road network
Previously an error could sometimes occur during city generation
due to attempting to build a road outside of the valid bounds of
the road network. This would cause the "Building new city..."
overlay to be shown forever, requiring a page refresh to use the
app again.

The culprit was `TerrainCandidateRoadNetwork.edgesFrom()`. When
returning the edges from a point it could include edges outside
the valid bounds of the road network. This could cause the chosen
path from a neighborhood center to the rest of the road network to
include invalid out of bounds points. When this path was converted
into actual roads the error would occur.

This error was more likely to occur when building a large city
with hilly terrain, because that would make it more likely a
neighborhood center was near the edge of the terrain and had
constrained options for connecting to the rest of the road
network.

Although `TerrainCandidateRoadNetwork.edgesFrom()` would correctly
only return edges that were inside the bounds of the terrain
(which has larger bounds than the road network), this was by
accident. When an edge was outside of the bounds certain values
would be `undefined` instead of a number, which accidentally
caused the "is this edge valid" conditions to evaluate to `false`.
A side benefit of this commit is that the correct behavior will
now instead happen more intentionally.

This bug appears to have been introduced in commit
26e9ecfed4960273be2819b3c621d30ac7c7ae9a. That commit should have
included the changes in this commit, but they were overlooked.
Although, you could argue they should have been added even earlier
to fix the "accidentally correct" behavior mentioned above.

src/terrain_candidate_road_network.js
@@ -32,33 +32,40 @@ var TerrainCandidateRoadNetwork = function(terrain, roadNetwork, maxRoadAngle) {
     var eastHeight = terrain.heightAt(x + 1, z);
     var eastAngle = Math.atan2((heightAtCurrentPoint - eastHeight), Config.BLOCK_WIDTH);
 
-    if (terrain.waterHeightAt(x, z - 1) > 0.0) {
-      addBridgeEdge(edges, x, z, 0, -1);
-    }
-    else if (Math.abs(northAngle) <= maxRoadAngle) {
-      edges.push({ destinationX: x, destinationZ: z - 1, edge: { distance: 1.0, gradeType: RoadNetwork.SURFACE_GRADE }});
-    }
-
-    if (terrain.waterHeightAt(x, z + 1) > 0.0) {
-      addBridgeEdge(edges, x, z, 0, 1);
-    }
-    else if (Math.abs(southAngle) <= maxRoadAngle) {
-      edges.push({ destinationX: x, destinationZ: z + 1, edge: { distance: 1.0, gradeType: RoadNetwork.SURFACE_GRADE }});
+    if (hasIntersection(x, z - 1) === true) {
+      if (terrain.waterHeightAt(x, z - 1) > 0.0) {
+        addBridgeEdge(edges, x, z, 0, -1);
+      }
+      else if (Math.abs(northAngle) <= maxRoadAngle) {
+        edges.push({ destinationX: x, destinationZ: z - 1, edge: { distance: 1.0, gradeType: RoadNetwork.SURFACE_GRADE }});
+      }
     }
 
+    if (hasIntersection(x, z + 1) === true) {
+      if (terrain.waterHeightAt(x, z + 1) > 0.0) {
+        addBridgeEdge(edges, x, z, 0, 1);
+      }
+      else if (Math.abs(southAngle) <= maxRoadAngle) {
+        edges.push({ destinationX: x, destinationZ: z + 1, edge: { distance: 1.0, gradeType: RoadNetwork.SURFACE_GRADE }});
+      }
+    }
 
-    if (terrain.waterHeightAt(x - 1, z) > 0.0) {
-      addBridgeEdge(edges, x, z, -1, 0);
-    }
-    else if (Math.abs(westAngle) <= maxRoadAngle) {
-      edges.push({ destinationX: x - 1, destinationZ: z, edge: { distance: 1.0, gradeType: RoadNetwork.SURFACE_GRADE }});
+    if (hasIntersection(x - 1, z) === true) {
+      if (terrain.waterHeightAt(x - 1, z) > 0.0) {
+        addBridgeEdge(edges, x, z, -1, 0);
+      }
+      else if (Math.abs(westAngle) <= maxRoadAngle) {
+        edges.push({ destinationX: x - 1, destinationZ: z, edge: { distance: 1.0, gradeType: RoadNetwork.SURFACE_GRADE }});
+      }
     }
 
-    if (terrain.waterHeightAt(x + 1, z) > 0.0) {
-      addBridgeEdge(edges, x, z, 1, 0);
-    }
-    else if (Math.abs(eastAngle) <= maxRoadAngle) {
-      edges.push({ destinationX: x + 1, destinationZ: z, edge: { distance: 1.0, gradeType: RoadNetwork.SURFACE_GRADE }});
+    if (hasIntersection(x + 1, z) === true) {
+      if (terrain.waterHeightAt(x + 1, z) > 0.0) {
+        addBridgeEdge(edges, x, z, 1, 0);
+      }
+      else if (Math.abs(eastAngle) <= maxRoadAngle) {
+        edges.push({ destinationX: x + 1, destinationZ: z, edge: { distance: 1.0, gradeType: RoadNetwork.SURFACE_GRADE }});
+      }
     }
 
     return edges;